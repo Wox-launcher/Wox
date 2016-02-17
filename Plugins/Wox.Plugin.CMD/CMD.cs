Wox-launcher/Wox

commit 1f939ff33962e994435eb634cded2f6c3a82b621

Refactoring directory path for plugins

Plugins/Wox.Plugin.CMD/CMD.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Windows.Forms;
 using WindowsInput;
 using WindowsInput.Native;
+using Wox.Infrastructure;
 using Wox.Infrastructure.Hotkey;
 using Wox.Infrastructure.Logger;
 using Control = System.Windows.Controls.Control;
@@ -202,7 +202,7 @@ namespace Wox.Plugin.CMD
 
         public string GetLanguagesFolder()
         {
-            return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Languages");
+            return Path.Combine(WoxDirectroy.Executable, "Languages");
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.Caculator/Calculator.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Windows;
+using Wox.Infrastructure;
 using YAMP;
 
 namespace Wox.Plugin.Caculator
@@ -95,7 +95,8 @@ namespace Wox.Plugin.Caculator
 
         public string GetLanguagesFolder()
         {
-            return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Languages");
+            return Path.Combine(WoxDirectroy.Executable, "Languages");
+
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.Color/Color.cs
@@ -4,8 +4,8 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Windows;
+using Wox.Infrastructure;
 
 namespace Wox.Plugin.Color
 {
@@ -111,7 +111,7 @@ namespace Wox.Plugin.Color
 
         public string GetLanguagesFolder()
         {
-            return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Languages");
+            return Path.Combine(WoxDirectroy.Executable, "Languages");
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.ControlPanel/ControlPanel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using Wox.Infrastructure;
 
 namespace Wox.Plugin.ControlPanel
@@ -85,7 +84,7 @@ namespace Wox.Plugin.ControlPanel
 
         public string GetLanguagesFolder()
         {
-            return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Languages");
+            return Path.Combine(WoxDirectroy.Executable, "Languages");
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.Everything/Main.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.ServiceProcess;
 using System.Windows;
+using Wox.Infrastructure;
 using Wox.Plugin.Everything.Everything;
 
 namespace Wox.Plugin.Everything
@@ -258,7 +258,7 @@ namespace Wox.Plugin.Everything
 
         public string GetLanguagesFolder()
         {
-            return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Languages");
+            return Path.Combine(WoxDirectroy.Executable, "Languages");
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.Folder/FolderPlugin.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
+using Wox.Infrastructure;
 
 namespace Wox.Plugin.Folder
 {
@@ -246,7 +246,7 @@ namespace Wox.Plugin.Folder
 
         public string GetLanguagesFolder()
         {
-            return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Languages");
+            return Path.Combine(WoxDirectroy.Executable, "Languages");
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.PluginIndicator/PluginIndicator.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using Wox.Core.Plugin;
 using Wox.Core.UserSettings;
+using Wox.Infrastructure;
 
 namespace Wox.Plugin.PluginIndicator
 {
@@ -41,7 +41,7 @@ namespace Wox.Plugin.PluginIndicator
 
         public string GetLanguagesFolder()
         {
-            return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Languages");
+            return Path.Combine(WoxDirectroy.Executable, "Languages");
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.PluginManagement/Main.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Windows;
 using Newtonsoft.Json;
+using Wox.Infrastructure;
 
 namespace Wox.Plugin.PluginManagement
 {
@@ -244,7 +244,7 @@ namespace Wox.Plugin.PluginManagement
 
         public string GetLanguagesFolder()
         {
-            return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Languages");
+            return Path.Combine(WoxDirectroy.Executable, "Languages");
         }
 
         public string GetTranslatedPluginTitle()

commit 30f13033d840216a98df2df4da196be7d1641d8a

Fix "NoTranslation" bug for plugins
1. fix #475
2. remove GetLanguagesFolder, part of #468
3. bug introduced since 1f939ff3

Plugins/Wox.Plugin.CMD/CMD.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Windows.Forms;
 using WindowsInput;
 using WindowsInput.Native;
-using Wox.Infrastructure;
 using Wox.Infrastructure.Hotkey;
 using Wox.Infrastructure.Logger;
 using Control = System.Windows.Controls.Control;
@@ -198,11 +197,6 @@ namespace Wox.Plugin.CMD
         public Control CreateSettingPanel()
         {
             return new CMDSetting();
-        }
-
-        public string GetLanguagesFolder()
-        {
-            return Path.Combine(WoxDirectroy.Executable, "Languages");
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.Caculator/Calculator.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Windows;
-using Wox.Infrastructure;
 using YAMP;
 
 namespace Wox.Plugin.Caculator
@@ -91,12 +89,6 @@ namespace Wox.Plugin.Caculator
         public void Init(PluginInitContext context)
         {
             this.context = context;
-        }
-
-        public string GetLanguagesFolder()
-        {
-            return Path.Combine(WoxDirectroy.Executable, "Languages");
-
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.Color/Color.cs
@@ -5,7 +5,6 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Windows;
-using Wox.Infrastructure;
 
 namespace Wox.Plugin.Color
 {
@@ -108,11 +107,6 @@ namespace Wox.Plugin.Color
             this.context = context;
         }
 
-
-        public string GetLanguagesFolder()
-        {
-            return Path.Combine(WoxDirectroy.Executable, "Languages");
-        }
 
         public string GetTranslatedPluginTitle()
         {

Plugins/Wox.Plugin.ControlPanel/ControlPanel.cs
@@ -82,11 +82,6 @@ namespace Wox.Plugin.ControlPanel
             return false;
         }
 
-        public string GetLanguagesFolder()
-        {
-            return Path.Combine(WoxDirectroy.Executable, "Languages");
-        }
-
         public string GetTranslatedPluginTitle()
         {
             return context.API.GetTranslation("wox_plugin_controlpanel_plugin_name");

Plugins/Wox.Plugin.Everything/Main.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.ServiceProcess;
 using System.Windows;
-using Wox.Infrastructure;
 using Wox.Plugin.Everything.Everything;
 
 namespace Wox.Plugin.Everything
@@ -254,11 +253,6 @@ namespace Wox.Plugin.Everything
                                             "PortableEverything",
                                             "Everything.exe");
             return directory;
-        }
-
-        public string GetLanguagesFolder()
-        {
-            return Path.Combine(WoxDirectroy.Executable, "Languages");
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.Folder/FolderPlugin.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using Wox.Infrastructure;
 
 namespace Wox.Plugin.Folder
 {
@@ -242,11 +241,6 @@ namespace Wox.Plugin.Folder
             }
 
             return results;
-        }
-
-        public string GetLanguagesFolder()
-        {
-            return Path.Combine(WoxDirectroy.Executable, "Languages");
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.PluginIndicator/PluginIndicator.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using Wox.Core.Plugin;
 using Wox.Core.UserSettings;
-using Wox.Infrastructure;
 
 namespace Wox.Plugin.PluginIndicator
 {
@@ -37,11 +35,6 @@ namespace Wox.Plugin.PluginIndicator
         public void Init(PluginInitContext context)
         {
             this.context = context;
-        }
-
-        public string GetLanguagesFolder()
-        {
-            return Path.Combine(WoxDirectroy.Executable, "Languages");
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.PluginManagement/Main.cs
@@ -7,7 +7,6 @@ using System.Text;
 using System.Threading;
 using System.Windows;
 using Newtonsoft.Json;
-using Wox.Infrastructure;
 
 namespace Wox.Plugin.PluginManagement
 {
@@ -240,11 +239,6 @@ namespace Wox.Plugin.PluginManagement
         public void Init(PluginInitContext context)
         {
             this.context = context;
-        }
-
-        public string GetLanguagesFolder()
-        {
-            return Path.Combine(WoxDirectroy.Executable, "Languages");
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.Program/Programs.cs
@@ -175,10 +175,6 @@ namespace Wox.Plugin.Program
 
         #endregion
 
-        public string GetLanguagesFolder()
-        {
-            return Path.Combine(WoxDirectroy.Executable, "Languages");
-        }
         public string GetTranslatedPluginTitle()
         {
             return context.API.GetTranslation("wox_plugin_program_plugin_name");

Plugins/Wox.Plugin.Sys/Sys.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Forms;
@@ -188,11 +187,6 @@ namespace Wox.Plugin.Sys
                     }
                 }
             });
-        }
-
-        public string GetLanguagesFolder()
-        {
-            return Path.Combine(WoxDirectroy.Executable, "Languages");
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.Url/UrlPlugin.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Text.RegularExpressions;
-using Wox.Infrastructure;
 
 namespace Wox.Plugin.Url
 {
@@ -101,11 +99,6 @@ namespace Wox.Plugin.Url
         public void Init(PluginInitContext context)
         {
             this.context = context;
-        }
-
-        public string GetLanguagesFolder()
-        {
-            return Path.Combine(WoxDirectroy.Executable, "Languages");
         }
 
         public string GetTranslatedPluginTitle()

Plugins/Wox.Plugin.WebSearch/WebSearchPlugin.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Windows.Controls;
-using Wox.Infrastructure;
 using Wox.Plugin.WebSearch.Annotations;
 using Wox.Plugin.WebSearch.SuggestionSources;
 
@@ -89,11 +87,6 @@ namespace Wox.Plugin.WebSearch
         }
 
         #endregion
-
-        public string GetLanguagesFolder()
-        {
-            return Path.Combine(WoxDirectroy.Executable, "Languages");
-        }
 
         public string GetTranslatedPluginTitle()
         {

Wox.Core/Resource/Internationalization.cs
@@ -15,10 +15,10 @@ namespace Wox.Core.Resource
         public Internationalization()
         {
             DirectoryName = "Languages";
-            MakesureThemeDirectoriesExist();
+            MakesureDirectoriesExist();
         }
 
-        private void MakesureThemeDirectoriesExist()
+        private void MakesureDirectoriesExist()
         {
             if (!Directory.Exists(DirectoryPath))
             {
@@ -73,10 +73,11 @@ namespace Wox.Core.Resource
 
         public override ResourceDictionary GetResourceDictionary()
         {
-            return new ResourceDictionary
+            var dictionary = new ResourceDictionary
             {
                 Source = new Uri(GetLanguageFile(DirectoryPath), UriKind.Absolute)
             };
+            return dictionary;
         }
 
         public List<Language> LoadAvailableLanguages()

Wox.Core/Resource/ResourceMerger.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
 using Wox.Core.Plugin;
 using Wox.Plugin;
+using Wox.Infrastructure.Exception;
 
 namespace Wox.Core.Resource
 {
@@ -33,14 +35,28 @@ namespace Wox.Core.Resource
         internal static void UpdatePluginLanguages()
         {
             RemoveResource(PluginManager.DirectoryName);
-            foreach (var languageFile in PluginManager.GetPluginsForInterface<IPluginI18n>().
-                Select(plugin => InternationalizationManager.Instance.GetLanguageFile(((IPluginI18n)plugin.Plugin).GetLanguagesFolder())).
-                Where(file => !string.IsNullOrEmpty(file)))
+            foreach (var plugin in PluginManager.GetPluginsForInterface<IPluginI18n>())
             {
-                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary
+                var location = Assembly.GetAssembly(plugin.Plugin.GetType()).Location;
+                var directoryName = Path.GetDirectoryName(location);
+                if (directoryName != null)
                 {
-                    Source = new Uri(languageFile, UriKind.Absolute)
-                });
+                    var internationalization = InternationalizationManager.Instance;
+                    var folder = Path.Combine(directoryName, internationalization.DirectoryName);
+                    var file = internationalization.GetLanguageFile(folder);
+                    if (!string.IsNullOrEmpty(file))
+                    {
+                        Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary
+                        {
+                            Source = new Uri(file, UriKind.Absolute)
+                        });
+                    }
+                }
+                else
+                {
+                    throw new WoxPluginException(plugin.Metadata.Name, "Can't find plugin location.");
+                }
+                
             }
         }
     }

Wox.Infrastructure/Exception/WoxPluginException.cs
@@ -14,5 +14,9 @@
         {
             PluginName = pluginName;
         }
+
+        public WoxPluginException(string msg) : base(msg)
+        {
+        }
     }
 }

Wox.Plugin/Feature.cs
@@ -32,8 +32,6 @@ namespace Wox.Plugin
     /// </summary>
     public interface IPluginI18n : IFeatures
     {
-        string GetLanguagesFolder();
-
         string GetTranslatedPluginTitle();
 
         string GetTranslatedPluginDescription();