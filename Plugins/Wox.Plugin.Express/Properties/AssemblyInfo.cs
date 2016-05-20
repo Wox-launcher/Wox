49b57c7081a33d221ba86c3f4a09ae4f386b029b
zxsean <zxsean@126.com>
2016-05-20 17:57:21 +0800

    [*]更新uuid.

--- Plugins/Wox.Plugin.Express/Properties/AssemblyInfo.cs
+++ Plugins/Wox.Plugin.Express/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Wox.Plugin.Express")]
-[assembly: Guid("ba698b70-59ed-4c2e-bce1-497eb2f9e76f")]
+[assembly: Guid("d4b98892-9dc7-448b-88cc-64cfa8f22ba4")]
