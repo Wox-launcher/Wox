344626b247482294786d40d0ea3f470266a8d477
zxsean <zxsean@126.com>
2016-05-20 16:21:05 +0800

    [+]快递查询.

---
+++ Plugins/Wox.Plugin.Express/ExpressModel.cs
@@ -0,0 +1,269 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Wox.Plugin.Express
+{
+    //public class ExpressModel
+    //{
+    //    /// <summary>
+    //    /// 快递名称
+    //    /// </summary>
+    //    [JsonProperty("name")]
+    //    public string Name { get; set; }
+
+    //    /// <summary>
+    //    /// 单号
+    //    /// </summary>
+    //    [JsonProperty("order")]
+    //    internal string Order { get; set; }
+
+    //    /// <summary>
+    //    /// 错误信息
+    //    /// </summary>
+    //    [JsonProperty("message")]
+    //    internal string Message { get; set; }
+
+    //    /// <summary>
+    //    /// 0000	接口调用正常,无任何错误
+    //    /// 0001	传输参数格式有误
+    //    /// 0002	用户编号(uid)无效
+    //    /// 0003	用户被禁用
+    //    /// 0004	key无效
+    //    /// 0005	快递代号(id)无效
+    //    /// 0006	访问次数达到最大额度
+    //    /// 0007	查询服务器返回错误
+    //    /// </summary>
+    //    [JsonProperty("errcode")]
+    //    internal string Errcode { get; set; }
+
+    //    /// <summary>
+    //    /// -1	待查询、在批量查询中才会出现的状态,指提交后还没有进行任何更新的单号
+    //    /// 0	查询异常
+    //    /// 1	暂无记录、单号没有任何跟踪记录
+    //    /// 2	在途中
+    //    /// 3	派送中
+    //    /// 4	已签收
+    //    /// 5	拒收、用户拒签
+    //    /// 6	疑难件、以为某些原因无法进行派送
+    //    /// 7	无效单
+    //    /// 8	超时单
+    //    /// 9	签收失败
+    //    /// </summary>
+    //    [JsonProperty("status")]
+    //    internal int Status { get; set; }
+
+    //    internal string getStatus()
+    //    {
+    //        switch (Status)
+    //        {
+    //            case -1:
+    //                {
+    //                    return "待查询";
+    //                }
+    //            case 0:
+    //                {
+    //                    return "查询异常";
+    //                }
+    //            case 1:
+    //                {
+    //                    return "暂无记录";
+    //                }
+    //            case 2:
+    //                {
+    //                    return "在途中";
+    //                }
+    //            case 3:
+    //                {
+    //                    return "派送中";
+    //                }
+    //            case 4:
+    //                {
+    //                    return "已签收";
+    //                }
+    //            case 5:
+    //                {
+    //                    return "用户拒签";
+    //                }
+    //            case 6:
+    //                {
+    //                    return "疑难件";
+    //                }
+    //            case 7:
+    //                {
+    //                    return "无效单";
+    //                }
+    //            case 8:
+    //                {
+    //                    return "超时单";
+    //                }
+    //            case 9:
+    //                {
+    //                    return "签收失败";
+    //                }
+    //        }
+
+    //        return Status.ToString();
+    //    }
+
+    //    [JsonProperty("data")]
+    //    public ExpressData[] Data { get; set; }
+    //}
+
+    //public class ExpressData
+    //{
+    //    [JsonProperty("time")]
+    //    public string Time { get; set; }
+
+    //    [JsonProperty("content")]
+    //    public string Content { get; set; }
+    //}
+
+    ////{"id":"yuantong","name":"圆通快递","order":"881754461664541778","message":"",
+    //// "errcode":"0000","status":4,"data":[{"time":"2016-05-09 20:16:31",
+    //// "content":"河北省石家庄市高新开发区公司(点击查询电话) 已揽收"},
+    ////{"time":"2016-05-09 22:38:39","content":"河北省石家庄市高新开发区公司 已打包"},
+    ////{"time":"2016-05-09 23:31:07","content":"河北省石家庄市高新开发区公司 已发出,下一站 石家庄转运中心"},
+    ////{"time":"2016-05-10 01:53:23","content":"石家庄转运中心 已收入"},
+    ////{"time":"2016-05-10 02:43:33","content":"石家庄转运中心 已发出,下一站 成都转运中心"},
+    ////{"time":"2016-05-11 07:07:28","content":"成都转运中心 已收入"},
+    ////{"time":"2016-05-11 08:22:51","content":"成都转运中心 已发出,下一站 四川省成都市双流县天府中和"},
+    ////{"time":"2016-05-11 12:51:58","content":"四川省成都市双流县天府中和公司 已收入"},
+    ////{"time":"2016-05-11 14:14:42","content":"四川省成都市双流县天府中和公司(点击查询电话)王** 派件中 派件员电话18280067758"},
+    ////{"time":"2016-05-11 14:15:26","content":"客户 签收人: 代办点友谊超市 已签收感谢使用圆通速递，期待再次为您服务"}]}
+
+    //public class ExpressDataInfo
+    //{
+    //    [JsonProperty("expresskey")]
+    //    public string Key { get; set; }
+
+    //    /// <summary>
+    //    /// 快递名
+    //    /// </summary>
+    //    [JsonProperty("expressname")]
+    //    public string Name { get; set; }
+    //}
+
+    public class ExpressModel
+    {
+        /// <summary>
+        /// 错误信息
+        /// </summary>
+        [JsonProperty("msg")]
+        internal string Message { get; set; }
+
+        /// <summary>
+        /// 错误代码
+        /// </summary>
+        [JsonProperty("status")]
+        internal int Status { get; set; }
+
+        [JsonProperty("data")]
+        public ExpressData Data { get; set; }
+    }
+
+    public class ExpressData
+    {
+        [JsonProperty("info")]
+        public ExpressInfo Info { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+    }
+
+    public class ExpressInfo
+    {
+        /// <summary>
+        /// 当前状态
+        /// "0": "在途，即货物处于运输过程中",
+        /// "1": "揽件，货物已由快递公司揽收并且产生了第一条跟踪信息",
+        /// "2": "疑难，货物寄送过程出了问题",
+        /// "3": "签收，收件人已签收",
+        /// "4": "退签，即货物由于用户拒签、超区等原因退回，而且发件人已经签收",
+        /// "5": "派件，即快递正在进行同城派件",
+        /// "6": "退回，货物正处于退回发件人的途中"
+        /// </summary>
+        [JsonProperty("state")]
+        public int State { get; set; }
+
+        /// <summary>
+        /// 获取快递状态
+        /// </summary>
+        /// <returns></returns>
+        internal string GetStatus()
+        {
+            switch (State)
+            {
+                case 0:
+                    {
+                        return "在途";
+                    }
+                case 1:
+                    {
+                        return "揽件";
+                    }
+                case 2:
+                    {
+                        return "疑难";
+                    }
+                case 3:
+                    {
+                        return "签收";
+                    }
+                case 4:
+                    {
+                        return "退签";
+                    }
+                case 5:
+                    {
+                        return "派件";
+                    }
+                case 6:
+                    {
+                        return "退回";
+                    }
+            }
+
+            return State.ToString();
+        }
+
+        /// <summary>
+        /// 公司
+        /// </summary>
+        [JsonProperty("com")]
+        public string Com { get; set; }
+
+        /// <summary>
+        /// 进度描述
+        /// </summary>
+        [JsonProperty("context")]
+        public ExpressContext[] Context { get; set; }
+    }
+
+    public class ExpressContext
+    {
+        [JsonProperty("time")]
+        private float time { get; set; }
+
+        public string Time()
+        {
+            DateTime _dt = new DateTime(1970, 1, 1, 8, 0, 0);
+
+            return _dt.AddSeconds(time).ToString();
+        }
+
+        [JsonProperty("desc")]
+        public string Desc { get; set; }
+    }
+
+    public class ExpressDataInfo
+    {
+        [JsonProperty("expresskey")]
+        public string Key { get; set; }
+
+        /// <summary>
+        /// 快递名
+        /// </summary>
+        [JsonProperty("expressname")]
+        public string Name { get; set; }
+    }
+}
---
+++ Plugins/Wox.Plugin.Express/Main.cs
@@ -0,0 +1,303 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Wox.Plugin.Express
+{
+    public class Main : IPlugin, IPluginI18n, IResultUpdated
+    {
+        private const string __URL = "http://api.open.baidu.com/pae/channel/data/asyncqury?appid=4001";
+
+        //private const string __KEYWORD = "kdcx";
+
+        private string m_keyword = "";
+
+        private const string __TIPS = @"请输入快递缩写,例如顺丰:shunfeng+空格单号";
+
+        private PluginInitContext context { get; set; }
+
+        /// <summary>
+        /// 快递信息
+        /// </summary>
+        private List<ExpressDataInfo> m_listInfo = new List<ExpressDataInfo>();
+
+        public event ResultUpdatedEventHandler ResultsUpdated;
+
+        public List<Result> Query(Query query)
+        {
+            if (context.CurrentPluginMetadata.ActionKeywords.Count > 0)
+            {
+                m_keyword = context.CurrentPluginMetadata.ActionKeywords[0];
+            }
+
+            if (query.RawQuery.StartsWith(m_keyword + " "))
+            {
+                List<Result> _list = new List<Result>();
+
+                // 判断快递名称
+                var _str = query.RawQuery.Substring(m_keyword.Length + 1);
+
+                // 需要查询的信息
+                string[] _info = _str.Split(' ');
+
+                if (_info.Length < 2 ||
+                    _info[1].Equals(""))
+                {
+                    _list.Add(new Result
+                    {
+                        Title = __TIPS,
+                        SubTitle = "",
+                        IcoPath = "txt",
+                        Score = 20,
+                        Action = e =>
+                        {
+                            return false;
+                        }
+                    });
+
+                    return _list;
+                }
+                else
+                {
+                    // 查询快递id
+                    string _id = _info[0];
+
+                    // 快递公司名
+                    string _expressName = "未知快递";
+
+                    foreach (ExpressDataInfo i in m_listInfo)
+                    {
+                        if (_id.Equals(i.Key))
+                        {
+                            _id = i.Key;
+                            _expressName = i.Name;
+
+                            break;
+                        }
+                    }
+
+                    // 单号
+                    string _order = _info[1];
+
+                    // 开始查询
+                    if (_order.Length > 0)
+                    {
+                        _list.Add(new Result
+                        {
+                            Title = "查询" + _expressName + "_单号:" + _order,
+                            SubTitle = "点击开始查询",
+                            IcoPath = "txt",
+                            Score = 20,
+                            Action = e =>
+                            {
+                                _list.AddRange(QueryExpress(_id, _order, query));
+
+                                return false;
+                            }
+                        });
+                    }
+
+                    return _list;
+                }
+            }
+
+            return new List<Result>();
+        }
+
+        public void Init(PluginInitContext context)
+        {
+            this.context = context;
+
+            // init Info
+            // string _filePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/Plugins/Wox.Plugin.Express/Resources/ExpressInfo.json";
+            string _filePath = Path.Combine(context.CurrentPluginMetadata.PluginDirectory, "Resources\\ExpressInfo.json");
+
+            if (File.Exists(_filePath))
+            {
+                string _txtStr = File.ReadAllText(_filePath);
+                object _object = JsonConvert.DeserializeObject<List<ExpressDataInfo>>(_txtStr);
+                m_listInfo = _object as List<ExpressDataInfo>;
+            }
+        }
+
+        public string GetTranslatedPluginTitle()
+        {
+            return context.API.GetTranslation("wox_plugin_express_plugin_name");
+        }
+
+        public string GetTranslatedPluginDescription()
+        {
+            return context.API.GetTranslation("wox_plugin_express_plugin_description");
+        }
+
+        #region 接口
+
+        /// <summary>
+        /// 查询快递
+        /// </summary>
+        /// <param name="_id">快递id</param>
+        /// <param name="_order">单号</param>
+        /// <returns></returns>
+        private List<Result> QueryExpress(string _id, string _order, Query _query)
+        {
+            List<Result> _list = new List<Result>();
+
+            HttpClientHandler _handler = new HttpClientHandler();
+
+            // 这里为false表示不采用HttpClient的默认Cookie,而是采用httpRequestmessage的Cookie
+            _handler.UseCookies = false;
+
+            using (var _client = new HttpClient(_handler))
+            {
+                string _cookie = GetCookie(_id, _order, _client);
+
+                var request = new HttpRequestMessage(HttpMethod.Get,
+                                                   __URL +
+                                                   //__PARAM +
+                                                   //@"&order=" + _order +
+                                                   //@"&id=" + _id);
+                                                   @"&com=" + _id +
+                                                   @"&nu=" + _order +
+                                                   @"&qq-pf-to=pcqq.c2c");
+
+                request.Headers.Add("Cookie", _cookie);
+
+                HttpResponseMessage respones = _client.SendAsync(request).Result;
+
+                var jsonString = respones.Content.ReadAsStringAsync().Result;
+
+                var _expressModel = JsonConvert.DeserializeObject<ExpressModel>(jsonString);
+
+                // 是否是错误
+                if (_expressModel.Status == 0)
+                {
+                    // 第一条状态信息
+                    Result _result = new Result
+                    {
+                        Title = "快递状态:" + _expressModel.Data.Info.GetStatus(),
+                        SubTitle = "Copy this text to the clipboard",
+                        IcoPath = "txt",
+                        Score = 20,
+                        Action = e =>
+                        {
+                            try
+                            {
+                                return false;
+                            }
+                            catch (System.Runtime.InteropServices.ExternalException)
+                            {
+                                return false;
+                            }
+                        }
+                    };
+
+                    _list.Add(_result);
+
+                    // 表单
+                    //for (int i = _expressModel.Data.Info.Context.Length - 1; i > 0; --i)
+                    for (int i = 0; i < _expressModel.Data.Info.Context.Length; ++i)
+                    {
+                        ExpressContext _data = _expressModel.Data.Info.Context[i];
+
+                        _result = new Result
+                        {
+                            Title = _data.Time() + "_" + _data.Desc,
+                            SubTitle = "Copy this text to the clipboard",
+                            IcoPath = "txt",
+                            Score = 20,
+                            Action = e =>
+                            {
+                                //context_.Api.HideAndClear();
+
+                                try
+                                {
+                                    Clipboard.SetText(_data.Time() + "_" + _data.Desc);
+
+                                    return true;
+                                }
+                                catch (System.Runtime.InteropServices.ExternalException)
+                                {
+                                    return false;
+                                }
+                            }
+                        };
+
+                        _list.Add(_result);
+                    }
+                }
+                else
+                {
+                    Result _result = new Result
+                    {
+                        Title = "查询出现了错误:" + _expressModel.Message,
+                        SubTitle = "Copy this text to the clipboard",
+                        IcoPath = "txt",
+                        Score = 20,
+                        Action = e =>
+                        {
+                            try
+                            {
+                                Clipboard.SetText(_expressModel.Message);
+
+                                return true;
+                            }
+                            catch (System.Runtime.InteropServices.ExternalException)
+                            {
+                                return false;
+                            }
+                        }
+                    };
+
+                    _list.Add(_result);
+                }
+
+                // 可以为空
+                ResultsUpdated?.Invoke(this, new ResultUpdatedEventArgs
+                {
+                    Results = _list,
+                    Query = _query
+                });
+
+                return _list;
+            }
+        }
+
+        private static string GetCookie(string _id, string _order, HttpClient _client)
+        {
+            var _msg = new HttpRequestMessage(HttpMethod.Get,
+                                              __URL +
+                                              @"&com=" + _id +
+                                              @"&nu=" + _order +
+                                              @"&qq-pf-to=pcqq.c2c");
+
+            HttpResponseMessage _respones = _client.SendAsync(_msg).Result;
+
+            IEnumerable<string> _cookies = _respones.Headers.GetValues("Set-Cookie");
+
+            string _cookie = "";
+
+            foreach (string _str in _cookies)
+            {
+                _cookie = GetCookieValue(_str);
+
+                break;
+            }
+
+            return _cookie;
+        }
+
+        private static string GetCookieValue(string _cookie)
+        {
+            Regex _regex = new Regex(".*?;");
+            Match _value = _regex.Match(_cookie);
+            string _cookieValue = _value.Groups[0].Value;
+
+            return _cookieValue.Substring(0, _cookieValue.Length - 1);
+        }
+
+        #endregion 接口
+    }
+}
