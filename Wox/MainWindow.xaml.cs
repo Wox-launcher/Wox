Wox-launcher/Wox

commit 2a3bf727be1c8bc185f1b436893b1c4385d982ac

Refactoring visibility related events
1. remove unnecessary events from MainViewModel
2. remove usage of Obsolete api (show, hide etc)
3. fix space problem in #660
4. part of #486
5. fix up/down key bug introduced in 92b7ca6a1bafd254e39ee92812ff691906cd85c1
6. fix #678

Wox/MainWindow.xaml
@@ -7,10 +7,6 @@
         xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="d"
         Title="Wox"
         Topmost="True"
-        Loaded="OnLoaded"
-        Closing="OnClosing"
-        Drop="OnDrop"
-        Deactivated="OnDeactivated"
         SizeToContent="Height"
         ResizeMode="NoResize"
         WindowStyle="None"
@@ -20,17 +16,22 @@
         Style="{DynamicResource WindowStyle}"
         Icon="Images\app.png"
         AllowsTransparency="True"
-        Left="{Binding Left, Mode=TwoWay, UpdateSourceTrigger=PropertyChanged}"
-        Top="{Binding Top, Mode=TwoWay, UpdateSourceTrigger=PropertyChanged}"
+        Loaded="OnLoaded"
+        Closing="OnClosing"
+        Drop="OnDrop"
+        SizeChanged="OnSizeChanged"
+        IsVisibleChanged="OnMainWindowVisible"
+        Deactivated="OnDeactivated"
+        PreviewKeyDown="OnKeyDown"
         Visibility="{Binding MainWindowVisibility, Mode=TwoWay, UpdateSourceTrigger=PropertyChanged}"
-        d:DataContext="{d:DesignInstance vm:MainViewModel}">
+        d:DataContext="{d:DesignInstance vm:MainViewModel}"
+        FocusManager.FocusedElement="{Binding ElementName=QueryTextBox}"
+        FocusManager.IsFocusScope="True">
     <Window.InputBindings>
         <KeyBinding Key="Escape" Command="{Binding EscCommand}"></KeyBinding>
         <KeyBinding Key="F1" Command="{Binding StartHelpCommand}"></KeyBinding>
         <KeyBinding Key="Tab" Command="{Binding SelectNextItemCommand}"></KeyBinding>
         <KeyBinding Key="Tab" Modifiers="Shift" Command="{Binding SelectPrevItemCommand}"></KeyBinding>
-        <KeyBinding Key="Down" Command="{Binding SelectNextItemCommand}"></KeyBinding>
-        <KeyBinding Key="Up" Command="{Binding SelectPrevItemCommand}"></KeyBinding>
         <KeyBinding Key="N" Modifiers="Ctrl" Command="{Binding SelectNextItemCommand}"></KeyBinding>
         <KeyBinding Key="J" Modifiers="Ctrl" Command="{Binding SelectNextItemCommand}"></KeyBinding>
         <KeyBinding Key="D" Modifiers="Ctrl" Command="{Binding SelectNextPageCommand}"></KeyBinding>
@@ -50,12 +51,16 @@
         <KeyBinding Key="D8" Modifiers="Alt" Command="{Binding OpenResultCommand}" CommandParameter="7"></KeyBinding>
         <KeyBinding Key="D9" Modifiers="Alt" Command="{Binding OpenResultCommand}" CommandParameter="8"></KeyBinding>
     </Window.InputBindings>
-    <Border Style="{DynamicResource WindowBorderStyle}" MouseDown="OnMouseDown">
+    <Border Style="{DynamicResource WindowBorderStyle}" MouseDown="OnMouseDown" >
         <StackPanel Orientation="Vertical">
             <TextBox Style="{DynamicResource QueryBoxStyle}"
                      Text="{Binding QueryText, Mode=TwoWay, UpdateSourceTrigger=PropertyChanged}"
                      PreviewDragOver="OnPreviewDragOver"
+                     IsVisibleChanged="OnQueryVisible"
+                     TextChanged="OnQueryChanged"
                      AllowDrop="True"
+                     Focusable="True"
+                     Visibility="Visible"
                      x:Name="QueryTextBox">
                 <TextBox.ContextMenu>
                     <ContextMenu>

Wox/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Windows;
-using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media.Animation;
 using System.Windows.Controls;
@@ -10,11 +9,10 @@ using Wox.Core.Resource;
 using Wox.Core.UserSettings;
 using Wox.Helper;
 using Wox.ViewModel;
+using Screen = System.Windows.Forms.Screen;
 using ContextMenu = System.Windows.Forms.ContextMenu;
-using DataFormats = System.Windows.DataFormats;
-using DragEventArgs = System.Windows.DragEventArgs;
 using MenuItem = System.Windows.Forms.MenuItem;
-using MessageBox = System.Windows.MessageBox;
+using NotifyIcon = System.Windows.Forms.NotifyIcon;
 
 namespace Wox
 {
@@ -26,29 +24,27 @@ namespace Wox
         private readonly Storyboard _progressBarStoryboard = new Storyboard();
         private Settings _settings;
         private NotifyIcon _notifyIcon;
+        private MainViewModel _viewModel;
 
         #endregion
 
         public MainWindow(Settings settings, MainViewModel mainVM)
         {
-            DataContext = mainVM;
             InitializeComponent();
+            DataContext = mainVM;
+            _viewModel = mainVM;
             _settings = settings;
         }
         public MainWindow()
         {
             InitializeComponent();
         }
+
         private void OnClosing(object sender, CancelEventArgs e)
         {
             _notifyIcon.Visible = false;
-            _settings.WindowLeft = Left;
-            _settings.WindowTop = Top;
-            var vm = (MainViewModel)DataContext;
-            vm.Save();
+            _viewModel.Save();
         }
-
-
 
         private void OnLoaded(object sender, RoutedEventArgs _)
         {
@@ -56,85 +52,20 @@ namespace Wox
             WindowIntelopHelper.DisableControlBox(this);
             ThemeManager.Instance.ChangeTheme(_settings.Theme);
             InitializeNotifyIcon();
-
-            var vm = (MainViewModel)DataContext;
-            RegisterEvents(vm);
-
-            // happlebao todo delete
-            vm.Left = GetWindowsLeft();
-            vm.Top = GetWindowsTop();
-            vm.MainWindowVisibility = _settings.HideOnStartup ? Visibility.Hidden : Visibility.Visible;
-        }
-
-        private void RegisterEvents(MainViewModel vm)
-        {
-            vm.TextBoxSelected += (o, e) => QueryTextBox.SelectAll();
-            vm.CursorMovedToEnd += (o, e) =>
-            {
-                QueryTextBox.Focus();
-                QueryTextBox.CaretIndex = QueryTextBox.Text.Length;
-            };
-            vm.PropertyChanged += (o, e) =>
-            {
-                if (e.PropertyName == nameof(vm.MainWindowVisibility))
-                {
-                    if (vm.MainWindowVisibility.IsVisible())
-                    {
-                        Activate();
-                        QueryTextBox.Focus();
-                        Left = GetWindowsLeft();
-                        Top = GetWindowsTop();
-                        _settings.ActivateTimes++;
-                    }
-                    else
-                    {
-                        _settings.WindowLeft = Left;
-                        _settings.WindowTop = Top;
-                    }
-                }
-            };
         }
 
         private void InitializeNotifyIcon()
         {
             _notifyIcon = new NotifyIcon { Text = Infrastructure.Constant.Wox, Icon = Properties.Resources.app, Visible = true };
-            _notifyIcon.Click += (o, e) => App.API.ShowApp();
+            _notifyIcon.Click += (o, e) => Visibility = Visibility.Visible;
             var open = new MenuItem(InternationalizationManager.Instance.GetTranslation("iconTrayOpen"));
-            open.Click += (o, e) => App.API.ShowApp();
+            open.Click += (o, e) => Visibility = Visibility.Visible;
             var setting = new MenuItem(InternationalizationManager.Instance.GetTranslation("iconTraySettings"));
             setting.Click += (o, e) => App.API.OpenSettingDialog();
             var exit = new MenuItem(InternationalizationManager.Instance.GetTranslation("iconTrayExit"));
             exit.Click += (o, e) => Close();
             MenuItem[] childen = { open, setting, exit };
             _notifyIcon.ContextMenu = new ContextMenu(childen);
-        }
-
-        private double GetWindowsLeft()
-        {
-            if (_settings.RememberLastLaunchLocation)
-            {
-                return _settings.WindowLeft;
-            }
-
-            var screen = Screen.FromPoint(System.Windows.Forms.Cursor.Position);
-            var dip1 = WindowIntelopHelper.TransformPixelsToDIP(this, screen.WorkingArea.X, 0);
-            var dip2 = WindowIntelopHelper.TransformPixelsToDIP(this, screen.WorkingArea.Width, 0);
-            var left = (dip2.X - ActualWidth) / 2 + dip1.X;
-            return left;
-        }
-
-        private double GetWindowsTop()
-        {
-            if (_settings.RememberLastLaunchLocation)
-            {
-                return _settings.WindowTop;
-            }
-
-            var screen = Screen.FromPoint(System.Windows.Forms.Cursor.Position);
-            var dip1 = WindowIntelopHelper.TransformPixelsToDIP(this, 0, screen.WorkingArea.Y);
-            var dip2 = WindowIntelopHelper.TransformPixelsToDIP(this, 0, screen.WorkingArea.Height);
-            var top = (dip2.Y - ActualHeight) / 4 + dip1.Y;
-            return top;
         }
 
         private void InitProgressbarAnimation()
@@ -146,21 +77,13 @@ namespace Wox
             _progressBarStoryboard.Children.Add(da);
             _progressBarStoryboard.Children.Add(da1);
             _progressBarStoryboard.RepeatBehavior = RepeatBehavior.Forever;
-            ProgressBar.Visibility = Visibility.Hidden;
             ProgressBar.BeginStoryboard(_progressBarStoryboard);
+            _viewModel.ProgressBarVisibility = Visibility.Hidden;
         }
 
         private void OnMouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left) DragMove();
-        }
-
-        private void OnDeactivated(object sender, EventArgs e)
-        {
-            if (_settings.HideWhenDeactive)
-            {
-                App.API.HideApp();
-            }
         }
 
         private void OnPreviewMouseButtonDown(object sender, MouseButtonEventArgs e)
@@ -173,17 +96,13 @@ namespace Wox
                 var result = (ResultViewModel)item?.DataContext;
                 if (result != null)
                 {
-                    var vm = DataContext as MainViewModel;
-                    if (vm != null)
+                    if (e.ChangedButton == MouseButton.Left)
                     {
-                        if (e.ChangedButton == MouseButton.Left)
-                        {
-                            vm.OpenResultCommand.Execute(null);
-                        }
-                        else if (e.ChangedButton == MouseButton.Right)
-                        {
-                            vm.LoadContextMenuCommand.Execute(null);
-                        }
+                        _viewModel.OpenResultCommand.Execute(null);
+                    }
+                    else if (e.ChangedButton == MouseButton.Right)
+                    {
+                        _viewModel.LoadContextMenuCommand.Execute(null);
                     }
                 }
             }
@@ -217,5 +136,110 @@ namespace Wox
         {
             App.API.OpenSettingDialog();
         }
+
+
+        private void OnDeactivated(object sender, EventArgs e)
+        {
+            if (_settings.HideWhenDeactive)
+            {
+                Hide();
+            }
+        }
+
+        private void OnQueryVisible(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var visible = (bool)e.NewValue;
+            if (visible)
+            {
+                // the Focusable and the IsVisible both needs to be true when set focus
+                // logical is set in xaml
+                QueryTextBox.Focus();
+
+                if (_viewModel.QueryTextSelected)
+                {
+                    QueryTextBox.SelectAll();
+                    _viewModel.QueryTextSelected = false;
+                }
+            }
+        }
+
+        private void OnQueryChanged(object sender, TextChangedEventArgs e)
+        {
+            QueryTextBox.CaretIndex = QueryTextBox.Text.Length;
+        }
+
+        private void OnMainWindowVisible(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var visible = (bool)e.NewValue;
+            if (visible)
+            {
+                SetWindowPosition();
+                _settings.ActivateTimes++;
+            }
+        }
+
+        private bool _startup = true;
+        private void SetWindowPosition()
+        {
+            if (!_settings.RememberLastLaunchLocation && !_startup)
+            {
+                Left = WindowLeft();
+                Top = WindowTop();
+            }
+            else
+            {
+                _startup = false;
+            }
+        }
+
+
+
+        /// <summary>
+        // used to set correct position on windows first startup
+        // since the actual width and actual height will be avaiable after this event
+        /// </summary>
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Left = WindowLeft();
+            Top = WindowTop();
+        }
+
+        private double WindowLeft()
+        {
+            var screen = Screen.FromPoint(System.Windows.Forms.Cursor.Position);
+            var dip1 = WindowIntelopHelper.TransformPixelsToDIP(this, screen.WorkingArea.X, 0);
+            var dip2 = WindowIntelopHelper.TransformPixelsToDIP(this, screen.WorkingArea.Width, 0);
+            var left = (dip2.X - ActualWidth) / 2 + dip1.X;
+            return left;
+        }
+
+        private double WindowTop()
+        {
+            var screen = Screen.FromPoint(System.Windows.Forms.Cursor.Position);
+            var dip1 = WindowIntelopHelper.TransformPixelsToDIP(this, 0, screen.WorkingArea.Y);
+            var dip2 = WindowIntelopHelper.TransformPixelsToDIP(this, 0, screen.WorkingArea.Height);
+            var top = (dip2.Y - QueryTextBox.ActualHeight) / 4 + dip1.Y;
+            return top;
+        }
+
+        /// <summary>
+        /// Register up and down key
+        /// todo: any way to put this in xaml ?
+        /// </summary>
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Down)
+            {
+                _viewModel.SelectNextItemCommand.Execute(null);
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Up)
+            {
+                _viewModel.SelectPrevItemCommand.Execute(null);
+                e.Handled = true;
+            }
+        }
+
+
     }
 }

Plugins/Wox.Plugin.PluginManagement/Main.cs
@@ -152,7 +152,6 @@ namespace Wox.Plugin.PluginManagement
                             if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
                             string filePath = Path.Combine(folder, Guid.NewGuid().ToString() + ".wox");
 
-                            context.API.StartLoadingBar();
                             string pluginUrl = APIBASE + "/media/" + r1.plugin_file;
 
                             try
@@ -168,7 +167,6 @@ namespace Wox.Plugin.PluginManagement
                                 return false;
                             }
                             context.API.InstallPlugin(filePath);
-                            context.API.StopLoadingBar();
                         }
                         return false;
                     }

Plugins/Wox.Plugin.Shell/Main.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Windows.Forms;
+using System.Windows;
 using WindowsInput;
-using WindowsInput.Native;
 using Wox.Infrastructure.Hotkey;
 using Wox.Infrastructure.Logger;
 using Wox.Infrastructure.Storage;
+using Application = System.Windows.Application;
 using Control = System.Windows.Controls.Control;
+using Keys = System.Windows.Forms.Keys;
 
 namespace Wox.Plugin.CMD
 {
@@ -191,7 +192,7 @@ namespace Wox.Plugin.CMD
             }
             else if (_settings.Shell == Shell.RunCommand)
             {
-                var parts = command.Split(new[] {' '}, 2);
+                var parts = command.Split(new[] { ' ' }, 2);
                 if (parts.Length == 2)
                 {
                     var filename = parts[0];
@@ -283,17 +284,16 @@ namespace Wox.Plugin.CMD
                 if (keyevent == (int)KeyEvent.WM_KEYUP && WinRStroked && vkcode == (int)Keys.LWin)
                 {
                     WinRStroked = false;
-                    keyboardSimulator.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.CONTROL);
                     return false;
                 }
             }
-            return true;
+            return false;
         }
 
         private void OnWinRPressed()
         {
-            context.API.ShowApp();
             context.API.ChangeQuery($"{context.CurrentPluginMetadata.ActionKeywords[0]}{Plugin.Query.TermSeperater}");
+            Application.Current.MainWindow.Visibility = Visibility.Visible;
         }
 
         public Control CreateSettingPanel()
@@ -311,8 +311,6 @@ namespace Wox.Plugin.CMD
             return context.API.GetTranslation("wox_plugin_cmd_plugin_description");
         }
 
-        public bool IsInstantQuery(string query) => false;
-
         public List<Result> LoadContextMenus(Result selectedResult)
         {
             return new List<Result>
@@ -322,7 +320,6 @@ namespace Wox.Plugin.CMD
                             Title = context.API.GetTranslation("wox_plugin_cmd_run_as_administrator"),
                             Action = c =>
                             {
-                                context.API.HideApp();
                                 Execute(selectedResult.Title, true);
                                 return true;
                             },

Plugins/Wox.Plugin.Sys/Main.cs
@@ -161,7 +161,7 @@ namespace Wox.Plugin.Sys
                     IcoPath = "Images\\app.png",
                     Action = c =>
                     {
-                        context.API.CloseApp();
+                        Application.Current.MainWindow.Close();
                         return true;
                     }
                 },

Wox.Infrastructure/Logger/Log.cs
@@ -3,6 +3,7 @@ using System.IO;
 using NLog;
 using NLog.Config;
 using NLog.Targets;
+using Wox.Infrastructure.Exception;
 
 namespace Wox.Infrastructure.Logger
 {

Wox.Plugin/IPublicAPI.cs
@@ -31,11 +31,13 @@ namespace Wox.Plugin
         /// Just change the query text, this won't raise search
         /// </summary>
         /// <param name="query"></param>
+        [Obsolete]
         void ChangeQueryText(string query, bool selectAll = false);
 
         /// <summary>
         /// Close Wox
         /// </summary>
+        [Obsolete]
         void CloseApp();
 
         /// <summary>
@@ -46,11 +48,13 @@ namespace Wox.Plugin
         /// <summary>
         /// Hide Wox
         /// </summary>
+        [Obsolete]
         void HideApp();
 
         /// <summary>
         /// Show Wox
         /// </summary>
+        [Obsolete]
         void ShowApp();
 
         /// <summary>
@@ -69,11 +73,13 @@ namespace Wox.Plugin
         /// <summary>
         /// Show loading animation
         /// </summary>
+        [Obsolete("automatically start")]
         void StartLoadingBar();
 
         /// <summary>
         /// Stop loading animation
         /// </summary>
+        [Obsolete("automatically stop")]
         void StopLoadingBar();
 
         /// <summary>

Wox/App.xaml.cs
@@ -8,7 +8,6 @@ using Wox.Core.UserSettings;
 using Wox.Helper;
 using Wox.Infrastructure.Image;
 using Wox.Infrastructure.Logger;
-using Wox.Infrastructure.Storage;
 using Wox.ViewModel;
 using Stopwatch = Wox.Infrastructure.Stopwatch;
 
@@ -61,7 +60,10 @@ namespace Wox
                 AutoStartup();
                 AutoUpdates();
 
-                window.Show();
+                if (!_settings.HideOnStartup)
+                {
+                    mainVM.MainWindowVisibility = Visibility.Visible;
+                }
             });
         }
 
@@ -116,7 +118,7 @@ namespace Wox
         [Conditional("RELEASE")]
         private static void RegisterAppDomainExceptions()
         {
-            
+
             AppDomain.CurrentDomain.UnhandledException += ErrorReporting.UnhandledExceptionHandle;
             AppDomain.CurrentDomain.FirstChanceException += (s, e) =>
             {
@@ -131,14 +133,14 @@ namespace Wox
             // but if sessionending is not called, exit won't be called when log off / shutdown
             if (!_disposed)
             {
-                Current.Dispatcher.Invoke(() => ((MainViewModel) Current.MainWindow?.DataContext)?.Save());
+                Current.Dispatcher.Invoke(() => ((MainViewModel)Current.MainWindow?.DataContext)?.Save());
                 _disposed = true;
             }
         }
 
         public void OnSecondAppStarted()
         {
-            API.ShowApp();
+            Current.MainWindow.Visibility = Visibility.Visible;
         }
     }
 }

commit 7f7962729625fa7d9af89df13e197790243de678

Fix #706 , bug introduced in 2a3bf727be1c8bc185f1b436893b1c4385d982ac

Wox/MainWindow.xaml
@@ -20,13 +20,10 @@
         Closing="OnClosing"
         Drop="OnDrop"
         SizeChanged="OnSizeChanged"
-        IsVisibleChanged="OnMainWindowVisible"
         Deactivated="OnDeactivated"
         PreviewKeyDown="OnKeyDown"
         Visibility="{Binding MainWindowVisibility, Mode=TwoWay, UpdateSourceTrigger=PropertyChanged}"
-        d:DataContext="{d:DesignInstance vm:MainViewModel}"
-        FocusManager.FocusedElement="{Binding ElementName=QueryTextBox}"
-        FocusManager.IsFocusScope="True">
+        d:DataContext="{d:DesignInstance vm:MainViewModel}">
     <Window.InputBindings>
         <KeyBinding Key="Escape" Command="{Binding EscCommand}"></KeyBinding>
         <KeyBinding Key="F1" Command="{Binding StartHelpCommand}"></KeyBinding>
@@ -56,10 +53,7 @@
             <TextBox Style="{DynamicResource QueryBoxStyle}"
                      Text="{Binding QueryText, Mode=TwoWay, UpdateSourceTrigger=PropertyChanged}"
                      PreviewDragOver="OnPreviewDragOver"
-                     IsVisibleChanged="OnQueryVisible"
-                     TextChanged="OnQueryChanged"
                      AllowDrop="True"
-                     Focusable="True"
                      Visibility="Visible"
                      x:Name="QueryTextBox">
                 <TextBox.ContextMenu>

Wox/MainWindow.xaml.cs
@@ -51,6 +51,29 @@ namespace Wox
             WindowIntelopHelper.DisableControlBox(this);
             ThemeManager.Instance.ChangeTheme(_settings.Theme);
             InitializeNotifyIcon();
+            InitProgressbarAnimation();
+
+            _viewModel.PropertyChanged += (o, e) =>
+            {
+                if (e.PropertyName == nameof(MainViewModel.MainWindowVisibility))
+                {
+                    if (_viewModel.MainWindowVisibility.IsVisible())
+                    {
+                        Activate();
+                        QueryTextBox.Focus();
+                        SetWindowPosition();
+                        _settings.ActivateTimes++;
+                        if (_viewModel.QueryTextSelected)
+                        {
+                            QueryTextBox.SelectAll();
+                            _viewModel.QueryTextSelected = false;
+                        }
+                    }
+                }
+            };
+            // since the default main window visibility is visible
+            // so we need set focus during startup
+            QueryTextBox.Focus();
         }
 
         private void InitializeNotifyIcon()
@@ -145,38 +168,6 @@ namespace Wox
             }
         }
 
-        private void OnQueryVisible(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            var visible = (bool)e.NewValue;
-            if (visible)
-            {
-                // the Focusable and the IsVisible both needs to be true when set focus
-                // logical is set in xaml
-                QueryTextBox.Focus();
-
-                if (_viewModel.QueryTextSelected)
-                {
-                    QueryTextBox.SelectAll();
-                    _viewModel.QueryTextSelected = false;
-                }
-            }
-        }
-
-        private void OnQueryChanged(object sender, TextChangedEventArgs e)
-        {
-            QueryTextBox.CaretIndex = QueryTextBox.Text.Length;
-        }
-
-        private void OnMainWindowVisible(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            var visible = (bool)e.NewValue;
-            if (visible)
-            {
-                SetWindowPosition();
-                _settings.ActivateTimes++;
-            }
-        }
-
         private bool _startup = true;
         private void SetWindowPosition()
         {
@@ -191,8 +182,6 @@ namespace Wox
             }
         }
 
-
-
         /// <summary>
         // used to set correct position on windows first startup
         // since the actual width and actual height will be avaiable after this event
@@ -201,7 +190,6 @@ namespace Wox
         {
             Left = WindowLeft();
             Top = WindowTop();
-            InitProgressbarAnimation();
         }
 
         private double WindowLeft()
@@ -239,7 +227,5 @@ namespace Wox
                 e.Handled = true;
             }
         }
-
-
     }
 }