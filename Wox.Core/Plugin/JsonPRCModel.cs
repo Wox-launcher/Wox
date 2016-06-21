Wox-launcher/Wox

commit d739920462b14c5eb41e54cb1f4fd4b46ca7874a

Refactoring proxy, part 2

Wox.Core/Plugin/JsonPRCModel.cs
@@ -100,18 +100,6 @@ namespace Wox.Core.Plugin
     /// </summary>
     public class JsonRPCServerRequestModel : JsonRPCRequestModel
     {
-        public IHttpProxy HttpProxy { get; set; }
-
-        public override string ToString()
-        {
-            string rpc = base.ToString();
-            if (HttpProxy != null)
-            {
-                rpc += string.Format(@",\""proxy\"":{{\""enabled\"":{0},\""server\"":\""{1}\"",\""port\"":{2},\""username\"":\""{3}\"",\""password\"":\""{4}\""}}",
-                    HttpProxy.Enabled.ToString().ToLower(), HttpProxy.Server, HttpProxy.Port, HttpProxy.UserName, HttpProxy.Password);
-            }
-            return rpc + "}";
-        }
     }
 
     /// <summary>

Plugins/Wox.Plugin.PluginManagement/Main.cs
@@ -113,7 +113,7 @@ namespace Wox.Plugin.PluginManagement
             string json;
             try
             {
-                json = Http.Get(pluginSearchUrl + pluginName, context.Proxy).Result;
+                json = Http.Get(pluginSearchUrl + pluginName).Result;
             }
             catch (WebException e)
             {
@@ -156,7 +156,7 @@ namespace Wox.Plugin.PluginManagement
 
                             try
                             {
-                                Http.Download(pluginUrl, filePath, context.Proxy);
+                                Http.Download(pluginUrl, filePath);
                             }
                             catch (WebException e)
                             {

Plugins/Wox.Plugin.WebSearch/Main.cs
@@ -104,7 +104,7 @@ namespace Wox.Plugin.WebSearch
 
         private async Task<IEnumerable<Result>> Suggestions(string keyword, string subtitle, WebSearch webSearch)
         {
-            var source = SuggestionSource.GetSuggestionSource(_settings.WebSearchSuggestionSource, Context);
+            var source = SuggestionSource.GetSuggestionSource(_settings.WebSearchSuggestionSource);
             if (source != null)
             {
                 var suggestions = await source.GetSuggestions(keyword);

Plugins/Wox.Plugin.WebSearch/SuggestionSources/Baidu.cs
@@ -24,7 +24,7 @@ namespace Wox.Plugin.WebSearch.SuggestionSources
             try
             {
                 const string api = "http://suggestion.baidu.com/su?json=1&wd=";
-                result = await Http.Get(api + Uri.EscapeUriString(query), Proxy, "GB2312");
+                result = await Http.Get(api + Uri.EscapeUriString(query), "GB2312");
             }
             catch (WebException e)
             {
@@ -60,10 +60,6 @@ namespace Wox.Plugin.WebSearch.SuggestionSources
             }
 
             return new List<string>();
-        }
-
-        public Baidu(IHttpProxy httpProxy) : base(httpProxy)
-        {
         }
     }
 }

Plugins/Wox.Plugin.WebSearch/SuggestionSources/Google.cs
@@ -19,7 +19,7 @@ namespace Wox.Plugin.WebSearch.SuggestionSources
             try
             {
                 const string api = "https://www.google.com/complete/search?output=chrome&q=";
-                result = await Http.Get(api + Uri.EscapeUriString(query), Proxy);
+                result = await Http.Get(api + Uri.EscapeUriString(query));
             }
             catch (WebException e)
             {
@@ -47,10 +47,6 @@ namespace Wox.Plugin.WebSearch.SuggestionSources
                 }
             }
             return new List<string>();
-        }
-
-        public Google(IHttpProxy httpProxy) : base(httpProxy)
-        {
         }
     }
 }

Plugins/Wox.Plugin.WebSearch/SuggestionSources/ISuggestionSource.cs
@@ -6,24 +6,18 @@ namespace Wox.Plugin.WebSearch.SuggestionSources
     public abstract class SuggestionSource
     {
         public virtual string Domain { get; set; }
-        public IHttpProxy Proxy { get; set; }
-
-        public SuggestionSource(IHttpProxy httpProxy)
-        {
-            Proxy = httpProxy;
-        }
 
         public abstract Task<List<string>> GetSuggestions(string query);
 
-        public static SuggestionSource GetSuggestionSource(string name, PluginInitContext context)
+        public static SuggestionSource GetSuggestionSource(string name)
         {
             switch (name.ToLower())
             {
                 case "google":
-                    return new Google(context.Proxy);
+                    return new Google();
 
                 case "baidu":
-                    return new Baidu(context.Proxy);
+                    return new Baidu();
 
                 default:
                     return null;

Wox.Core/Plugin/ExecutablePlugin.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using Wox.Core.UserSettings;
 using Wox.Plugin;
 
 namespace Wox.Core.Plugin
@@ -28,7 +27,6 @@ namespace Wox.Core.Plugin
             {
                 Method = "query",
                 Parameters = new object[] { query.Search },
-                HttpProxy = HttpProxy.Instance
             };
 
             _startInfo.Arguments = $"\"{request}\"";

Wox.Core/Plugin/PluginManager.cs
@@ -5,11 +5,11 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Wox.Core.Resource;
-using Wox.Core.UserSettings;
 using Wox.Infrastructure;
 using Wox.Infrastructure.Exception;
 using Wox.Infrastructure.Logger;
 using Wox.Infrastructure.Storage;
+using Wox.Infrastructure.UserSettings;
 using Wox.Plugin;
 
 namespace Wox.Core.Plugin
@@ -83,7 +83,6 @@ namespace Wox.Core.Plugin
                     pair.Plugin.Init(new PluginInitContext
                     {
                         CurrentPluginMetadata = pair.Metadata,
-                        Proxy = HttpProxy.Instance,
                         API = API
                     });
                 });

commit 07d89e5cf9d3539b5d2f8d37890c4777ecc1275b

Fix python plugin bug introduced in d7399204

Wox.Core/Plugin/JsonPRCModel.cs
@@ -100,6 +100,11 @@ namespace Wox.Core.Plugin
     /// </summary>
     public class JsonRPCServerRequestModel : JsonRPCRequestModel
     {
+        public override string ToString()
+        {
+            string rpc = base.ToString();
+            return rpc + "}";
+        }
     }
 
     /// <summary>