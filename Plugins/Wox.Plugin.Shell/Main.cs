Wox-launcher/Wox

commit 2a3bf727be1c8bc185f1b436893b1c4385d982ac

Refactoring visibility related events
1. remove unnecessary events from MainViewModel
2. remove usage of Obsolete api (show, hide etc)
3. fix space problem in #660
4. part of #486
5. fix up/down key bug introduced in 92b7ca6a1bafd254e39ee92812ff691906cd85c1
6. fix #678

Plugins/Wox.Plugin.Shell/Main.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Windows.Forms;
+using System.Windows;
 using WindowsInput;
-using WindowsInput.Native;
 using Wox.Infrastructure.Hotkey;
 using Wox.Infrastructure.Logger;
 using Wox.Infrastructure.Storage;
+using Application = System.Windows.Application;
 using Control = System.Windows.Controls.Control;
+using Keys = System.Windows.Forms.Keys;
 
 namespace Wox.Plugin.CMD
 {
@@ -191,7 +192,7 @@ namespace Wox.Plugin.CMD
             }
             else if (_settings.Shell == Shell.RunCommand)
             {
-                var parts = command.Split(new[] {' '}, 2);
+                var parts = command.Split(new[] { ' ' }, 2);
                 if (parts.Length == 2)
                 {
                     var filename = parts[0];
@@ -283,17 +284,16 @@ namespace Wox.Plugin.CMD
                 if (keyevent == (int)KeyEvent.WM_KEYUP && WinRStroked && vkcode == (int)Keys.LWin)
                 {
                     WinRStroked = false;
-                    keyboardSimulator.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.CONTROL);
                     return false;
                 }
             }
-            return true;
+            return false;
         }
 
         private void OnWinRPressed()
         {
-            context.API.ShowApp();
             context.API.ChangeQuery($"{context.CurrentPluginMetadata.ActionKeywords[0]}{Plugin.Query.TermSeperater}");
+            Application.Current.MainWindow.Visibility = Visibility.Visible;
         }
 
         public Control CreateSettingPanel()
@@ -311,8 +311,6 @@ namespace Wox.Plugin.CMD
             return context.API.GetTranslation("wox_plugin_cmd_plugin_description");
         }
 
-        public bool IsInstantQuery(string query) => false;
-
         public List<Result> LoadContextMenus(Result selectedResult)
         {
             return new List<Result>
@@ -322,7 +320,6 @@ namespace Wox.Plugin.CMD
                             Title = context.API.GetTranslation("wox_plugin_cmd_run_as_administrator"),
                             Action = c =>
                             {
-                                context.API.HideApp();
                                 Execute(selectedResult.Title, true);
                                 return true;
                             },

Plugins/Wox.Plugin.PluginManagement/Main.cs
@@ -152,7 +152,6 @@ namespace Wox.Plugin.PluginManagement
                             if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
                             string filePath = Path.Combine(folder, Guid.NewGuid().ToString() + ".wox");
 
-                            context.API.StartLoadingBar();
                             string pluginUrl = APIBASE + "/media/" + r1.plugin_file;
 
                             try
@@ -168,7 +167,6 @@ namespace Wox.Plugin.PluginManagement
                                 return false;
                             }
                             context.API.InstallPlugin(filePath);
-                            context.API.StopLoadingBar();
                         }
                         return false;
                     }

Plugins/Wox.Plugin.Sys/Main.cs
@@ -161,7 +161,7 @@ namespace Wox.Plugin.Sys
                     IcoPath = "Images\\app.png",
                     Action = c =>
                     {
-                        context.API.CloseApp();
+                        Application.Current.MainWindow.Close();
                         return true;
                     }
                 },

Wox.Infrastructure/Logger/Log.cs
@@ -3,6 +3,7 @@ using System.IO;
 using NLog;
 using NLog.Config;
 using NLog.Targets;
+using Wox.Infrastructure.Exception;
 
 namespace Wox.Infrastructure.Logger
 {

Wox.Plugin/IPublicAPI.cs
@@ -31,11 +31,13 @@ namespace Wox.Plugin
         /// Just change the query text, this won't raise search
         /// </summary>
         /// <param name="query"></param>
+        [Obsolete]
         void ChangeQueryText(string query, bool selectAll = false);
 
         /// <summary>
         /// Close Wox
         /// </summary>
+        [Obsolete]
         void CloseApp();
 
         /// <summary>
@@ -46,11 +48,13 @@ namespace Wox.Plugin
         /// <summary>
         /// Hide Wox
         /// </summary>
+        [Obsolete]
         void HideApp();
 
         /// <summary>
         /// Show Wox
         /// </summary>
+        [Obsolete]
         void ShowApp();
 
         /// <summary>
@@ -69,11 +73,13 @@ namespace Wox.Plugin
         /// <summary>
         /// Show loading animation
         /// </summary>
+        [Obsolete("automatically start")]
         void StartLoadingBar();
 
         /// <summary>
         /// Stop loading animation
         /// </summary>
+        [Obsolete("automatically stop")]
         void StopLoadingBar();
 
         /// <summary>

Wox/App.xaml.cs
@@ -8,7 +8,6 @@ using Wox.Core.UserSettings;
 using Wox.Helper;
 using Wox.Infrastructure.Image;
 using Wox.Infrastructure.Logger;
-using Wox.Infrastructure.Storage;
 using Wox.ViewModel;
 using Stopwatch = Wox.Infrastructure.Stopwatch;
 
@@ -61,7 +60,10 @@ namespace Wox
                 AutoStartup();
                 AutoUpdates();
 
-                window.Show();
+                if (!_settings.HideOnStartup)
+                {
+                    mainVM.MainWindowVisibility = Visibility.Visible;
+                }
             });
         }
 
@@ -116,7 +118,7 @@ namespace Wox
         [Conditional("RELEASE")]
         private static void RegisterAppDomainExceptions()
         {
-            
+
             AppDomain.CurrentDomain.UnhandledException += ErrorReporting.UnhandledExceptionHandle;
             AppDomain.CurrentDomain.FirstChanceException += (s, e) =>
             {
@@ -131,14 +133,14 @@ namespace Wox
             // but if sessionending is not called, exit won't be called when log off / shutdown
             if (!_disposed)
             {
-                Current.Dispatcher.Invoke(() => ((MainViewModel) Current.MainWindow?.DataContext)?.Save());
+                Current.Dispatcher.Invoke(() => ((MainViewModel)Current.MainWindow?.DataContext)?.Save());
                 _disposed = true;
             }
         }
 
         public void OnSecondAppStarted()
         {
-            API.ShowApp();
+            Current.MainWindow.Visibility = Visibility.Visible;
         }
     }
 }

Wox/CustomQueryHotkeySetting.xaml.cs
@@ -55,7 +55,7 @@ namespace Wox
                 SetHotkey(ctlHotkey.CurrentHotkey, delegate
                 {
                     App.API.ChangeQuery(pluginHotkey.ActionKeyword);
-                    App.API.ShowApp();
+                    Application.Current.MainWindow.Visibility = Visibility.Visible;
                 });
                 MessageBox.Show(InternationalizationManager.Instance.GetTranslation("succeed"));
             }
@@ -73,8 +73,8 @@ namespace Wox
                 RemoveHotkey(oldHotkey);
                 SetHotkey(new HotkeyModel(updateCustomHotkey.Hotkey), delegate
                 {
-                    App.API.ShowApp();
                     App.API.ChangeQuery(updateCustomHotkey.ActionKeyword);
+                    Application.Current.MainWindow.Visibility = Visibility.Visible;
                 });
                 MessageBox.Show(InternationalizationManager.Instance.GetTranslation("succeed"));
             }
@@ -100,8 +100,8 @@ namespace Wox
 
         private void BtnTestActionKeyword_OnClick(object sender, RoutedEventArgs e)
         {
-            App.API.ShowApp();
             App.API.ChangeQuery(tbAction.Text);
+            Application.Current.MainWindow.Visibility = Visibility.Visible;
         }
 
         private void RemoveHotkey(string hotkeyStr)

Wox/Helper/SingletonWindowOpener.cs
@@ -10,7 +10,7 @@ namespace Wox.Helper
         {
             var window = Application.Current.Windows.OfType<Window>().FirstOrDefault(x => x.GetType() == typeof(T))
                          ?? (T)Activator.CreateInstance(typeof(T), args);
-            App.API.HideApp();
+            Application.Current.MainWindow.Hide();
             window.Show();
             window.Focus();
 

commit 3664b98206156239c469607770eca07c5446f167

fix bug introduced in 2a3bf727be1c8bc185f1b436893b1c4385d982ac

Plugins/Wox.Plugin.Shell/Main.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using WindowsInput;
+using WindowsInput.Native;
 using Wox.Infrastructure.Hotkey;
 using Wox.Infrastructure.Logger;
 using Wox.Infrastructure.Storage;
@@ -285,6 +286,7 @@ namespace Wox.Plugin.Shell
                 if (keyevent == (int)KeyEvent.WM_KEYUP && _winRStroked && vkcode == (int)Keys.LWin)
                 {
                     _winRStroked = false;
+                    _keyboardSimulator.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.CONTROL);
                     return false;
                 }
             }