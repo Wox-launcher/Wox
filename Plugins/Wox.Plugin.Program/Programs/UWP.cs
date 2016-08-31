c4a15183265cd5541c96fa6a4f9bcc7efd3f944e
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2016-08-31 22:15:31 +1000

    Include WinJS applications in UWP indexing.

--- Plugins/Wox.Plugin.Program/Programs/UWP.cs
+++ Plugins/Wox.Plugin.Program/Programs/UWP.cs
@@ -47,8 +47,7 @@ namespace Wox.Plugin.Program.Programs
             InitializeAppInfo();
             Apps = Apps.Where(a =>
             {
-                var valid = !string.IsNullOrEmpty(a.Executable) &&
-                            !string.IsNullOrEmpty(a.UserModelId) &&
+                var valid = !string.IsNullOrEmpty(a.UserModelId) &&
                             !string.IsNullOrEmpty(a.DisplayName);
                 return valid;
             }).ToArray();
@@ -81,7 +80,6 @@ namespace Wox.Plugin.Program.Programs
                     if (appListEntry != "nonoe")
                     {
                         Apps[i].UserModelId = currentApp.GetAppUserModelId();
-                        Apps[i].Executable = currentApp.GetStringValue("Executable") ?? string.Empty;
                         Apps[i].BackgroundColor = currentApp.GetStringValue("BackgroundColor") ?? string.Empty;
                         Apps[i].LogoPath = Path.Combine(Location, currentApp.GetStringValue("Square44x44Logo"));
                         Apps[i].Location = Location;
@@ -208,7 +206,6 @@ namespace Wox.Plugin.Program.Programs
             public string Description { get; set; }
             public RandomAccessStreamReference LogoStream { get; set; }
             public string UserModelId { get; set; }
-            public string Executable { get; set; }
             public string PublisherDisplayName { get; set; }
             public string BackgroundColor { get; set; }
             public string LogoPath { get; set; }
