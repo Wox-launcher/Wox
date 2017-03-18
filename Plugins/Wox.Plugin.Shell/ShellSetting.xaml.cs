dec7252eec416b7ff9a6c4cf8ecad4a8558fcad5
Zepp Lu <luzepu678@gmail.com>
2017-03-18 13:43:07 +0800

    Wox.Plugin.Shell now supports bash on Windows 10 #1308

--- Plugins/Wox.Plugin.Shell/ShellSetting.xaml.cs
+++ Plugins/Wox.Plugin.Shell/ShellSetting.xaml.cs
@@ -50,6 +50,10 @@ namespace Wox.Plugin.Shell
             };
 
             ShellComboBox.SelectedIndex = (int) _settings.Shell;
+            if (_settings.SupportWSL)
+            {
+                ShellComboBox.Items.Add("Bash");
+            }
             ShellComboBox.SelectionChanged += (o, e) =>
             {
                 _settings.Shell = (Shell) ShellComboBox.SelectedIndex;
