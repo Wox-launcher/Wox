7746319a7d9a50e44648b2bb02d8724b9493dc40
chenkn <chenkn@glodon.com>
2018-01-22 18:54:58 +0800

    settings window style and some resources

---
+++ Wox/DP/TabItemDependencyProperty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace Wox.DP
+{
+    public class TabItemDependencyProperty
+    {
+        public static DependencyProperty TabItemIconProperty = DependencyProperty.RegisterAttached("Icon", typeof(Uri),
+            typeof(TabItemDependencyProperty));
+
+        public static Uri GetTabItemIcon(DependencyObject obj)
+        {
+            return (Uri)obj.GetValue(TabItemIconProperty);
+        }
+
+        public static void SetTabItemIcon(DependencyObject obj, Uri value)
+        {
+            obj.SetValue(TabItemIconProperty, value);
+        }
+    }
+}
