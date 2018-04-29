bec538554de5f27cb636ec856f3f34f8c44525cc
Stephen Alderman <stephen.alderman@imgtec.com>
2018-04-30 00:09:47 +0100

    Change Python Plugins to send messages over stdin rather than argv[1]. Python start up times can be slow especially with plugins that have a lot of imports. To improve responsiveness rather than executing Python on every call (which is typically once per character typed), instead reuse the same python process and communicate over standard input.
    To avoid deadlocks the following conditions are now required:
    * All messages sent are require a single line response
    * Even no response requires an empty response
    * Maximum time between lines for error messages to be captured is 1s
    * All python plugins must be able to return to commands without hanging
    
    Threads can now be used to spawn long running actions without wox hanging
    Plugins that require logging into sites can retain that information in
    memory between messages.
    
    Python is spawned the first time the plugin is called, subsequent calls
    use the same python instance. If the python plugin is exited then it is
    respawned.

--- Wox.Core/Plugin/PythonPlugin.cs
+++ Wox.Core/Plugin/PythonPlugin.cs
@@ -1,31 +1,130 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Wox.Infrastructure;
+using Wox.Infrastructure.Logger;
 using Wox.Plugin;
+using System.Collections.Concurrent;
+using System.Text;
 
 namespace Wox.Core.Plugin
 {
     internal class PythonPlugin : JsonRPCPlugin
     {
-        private readonly ProcessStartInfo _startInfo;
+        //private readonly ProcessStartInfo _startInfo;
         public override string SupportedLanguage { get; set; } = AllowedLanguage.Python;
+        
+        private readonly Process _pythonProcess;
+        private readonly JsonSerializerSettings _jsonSerializerSettings;
+        private readonly BlockingCollection<string> outputBuffer, errorBuffer;
 
         public PythonPlugin(string filename)
         {
-            _startInfo = new ProcessStartInfo
+            _pythonProcess = new Process
             {
-                FileName = filename,
-                UseShellExecute = false,
-                CreateNoWindow = true,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = filename,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                }
             };
 
             // temp fix for issue #667
             var path = Path.Combine(Constant.ProgramDirectory, JsonRPC);
-            _startInfo.EnvironmentVariables["PYTHONPATH"] = path;
+            _pythonProcess.StartInfo.EnvironmentVariables["PYTHONPATH"] = path;
 
+            _pythonProcess.OutputDataReceived += new DataReceivedEventHandler((sender, e) => outputBuffer.Add(e.Data));
+            _pythonProcess.ErrorDataReceived += new DataReceivedEventHandler((sender, e) => errorBuffer.Add(e.Data));
+
+            _jsonSerializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new DefaultContractResolver
+                {
+                    NamingStrategy = new CamelCaseNamingStrategy
+                    {
+                        OverrideSpecifiedNames = false
+                    }
+                }
+            };
+            outputBuffer = new BlockingCollection<string>(new ConcurrentQueue<string>());
+            errorBuffer = new BlockingCollection<string>(new ConcurrentQueue<string>());
+        }
+
+        private bool IsRunning()
+        {
+            try
+            {
+                return (!_pythonProcess.HasExited && _pythonProcess.Id != 0);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private string SendToProcess(string request)
+        {
+            if (!IsRunning())
+            {
+                try
+                {
+                    _pythonProcess.CancelOutputRead();
+                    _pythonProcess.CancelErrorRead();
+                }
+                catch (InvalidOperationException) { }
+                _pythonProcess.StartInfo.Arguments = $"-B \"{context.CurrentPluginMetadata.ExecuteFilePath}\"";
+                _pythonProcess.StartInfo.WorkingDirectory = context.CurrentPluginMetadata.PluginDirectory;
+                _pythonProcess.Start();
+                _pythonProcess.BeginOutputReadLine();
+                _pythonProcess.BeginErrorReadLine();
+            }
+            
+            StreamWriter standardInput = _pythonProcess.StandardInput;
+
+            standardInput.AutoFlush = true;
+
+            while (outputBuffer.TryTake(out string line)) ; //Empty stdout before we start
+            standardInput.WriteLine(request);
+            if (!outputBuffer.TryTake(out string result, -1))
+            {
+                result = string.Empty;
+            }
+
+            if (string.IsNullOrEmpty(result))
+            {
+                StringBuilder errorBuild = new StringBuilder("");
+                while (errorBuffer.TryTake(out string line, 1000) && line != null)
+                {
+                    errorBuild.AppendLine(line);
+                }
+                string error = errorBuild.ToString();
+                if (!string.IsNullOrEmpty(error))
+                {
+                    Log.Error($"|JsonRPCPlugin.Execute|{error}");
+                    return string.Empty;
+                }
+                else
+                {
+                    Log.Error("|JsonRPCPlugin.Execute|Empty standard output and standard error.");
+                    return string.Empty;
+                }
+            }
+            else if (result.StartsWith("DEBUG:"))
+            {
+                MessageBox.Show(new Form { TopMost = true }, result.Substring(6));
+                return string.Empty;
+            }
+            else
+            {
+                return result;
+            }
         }
 
         protected override string ExecuteQuery(Query query)
@@ -35,19 +134,12 @@ namespace Wox.Core.Plugin
                 Method = "query",
                 Parameters = new object[] { query.Search },
             };
-            //Add -B flag to tell python don't write .py[co] files. Because .pyc contains location infos which will prevent python portable
-            _startInfo.Arguments = $"-B \"{context.CurrentPluginMetadata.ExecuteFilePath}\" \"{request}\"";
-            // todo happlebao why context can't be used in constructor
-            _startInfo.WorkingDirectory = context.CurrentPluginMetadata.PluginDirectory;
-
-            return Execute(_startInfo);
+            return SendToProcess(JsonConvert.SerializeObject(request, _jsonSerializerSettings));
         }
 
         protected override string ExecuteCallback(JsonRPCRequestModel rpcRequest)
         {
-            _startInfo.Arguments = $"-B \"{context.CurrentPluginMetadata.ExecuteFilePath}\" \"{rpcRequest}\"";
-            _startInfo.WorkingDirectory = context.CurrentPluginMetadata.PluginDirectory;
-            return Execute(_startInfo);
+            return SendToProcess(JsonConvert.SerializeObject(rpcRequest, _jsonSerializerSettings));
         }
 
         protected override string ExecuteContextMenu(Result selectedResult) {
@@ -55,10 +147,7 @@ namespace Wox.Core.Plugin
                 Method = "context_menu",
                 Parameters = new object[] { selectedResult.ContextData },
             };
-            _startInfo.Arguments = $"-B \"{context.CurrentPluginMetadata.ExecuteFilePath}\" \"{request}\"";
-            _startInfo.WorkingDirectory = context.CurrentPluginMetadata.PluginDirectory;
-
-            return Execute(_startInfo);
+            return SendToProcess(JsonConvert.SerializeObject(request, _jsonSerializerSettings));
         }
     }
 }
