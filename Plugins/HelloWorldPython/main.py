f45bc7f63d3e60ae8900b714bf61ff774e20012d
Prateek Rokadiya <prateekro@gmail.com>
2019-05-23 05:50:20 -0700

    Updated docs for easier understanding to develop python based plugins

--- Plugins/HelloWorldPython/main.py
+++ Plugins/HelloWorldPython/main.py
@@ -4,16 +4,23 @@ from wox import Wox
 
 class HelloWorld(Wox):
 
+    # query is default function to receive realtime keystrokes from wox launcher
     def query(self, query):
         results = []
         results.append({
             "Title": "Hello World",
             "SubTitle": "Query: {}".format(query),
             "IcoPath":"Images/app.ico",
-            "ContextData": "ctxData"
+            "ContextData": "ctxData",
+            "JsonRPCAction": {
+                'method': 'take_action',
+                'parameters': ["{}".format("SomeData")],
+                'dontHideAfterAction': False
+            }
         })
         return results
 
+    # context_menu is default function called for ContextData where `data = ctxData`
     def context_menu(self, data):
         results = []
         results.append({
@@ -22,6 +29,12 @@ class HelloWorld(Wox):
             "IcoPath":"Images/app.ico"
         })
         return results
+
+    def take_action(self, SomeArgument):
+        # Choose what to trigger on pressing enter on the result.
+        # use SomeArgument to do something with data sent by parameters.
+
+        return None
 
 if __name__ == "__main__":
     HelloWorld()
