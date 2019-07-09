45ff4fdaed986b386981706fbc688a74b33be778
liberize <liberize@gmail.com>
2019-07-09 23:36:19 +0800

    Add support for command line args `-q|--query`

--- Wox/App.xaml.cs
+++ Wox/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
+using System.Collections.Generic;
 using Wox.Core;
 using Wox.Core.Configuration;
 using Wox.Core.Plugin;
@@ -15,6 +16,7 @@ using Wox.Infrastructure.Logger;
 using Wox.Infrastructure.UserSettings;
 using Wox.ViewModel;
 using Stopwatch = Wox.Infrastructure.Stopwatch;
+using CommandLine;
 
 namespace Wox
 {
@@ -30,6 +32,25 @@ namespace Wox
         private readonly Portable _portable = new Portable();
         private readonly Alphabet _alphabet = new Alphabet();
         private StringMatcher _stringMatcher;
+
+        private class Options
+        {
+            [Option('q', "query", Required = false, HelpText = "Specify text to query on startup.")]
+            public string QueryText { get; set; }
+        }
+
+        private void ParseCommandLineArgs(IList<string> args)
+        {
+            if (args == null)
+                return;
+
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o =>
+                {
+                    if (o.QueryText != null && _mainVM != null)
+                        _mainVM.ChangeQueryText(o.QueryText);
+                });
+        }
 
         [STAThread]
         public static void Main()
@@ -90,6 +111,7 @@ namespace Wox
                 AutoStartup();
                 AutoUpdates();
 
+                ParseCommandLineArgs(SingleInstance<App>.CommandLineArgs);
                 _mainVM.MainWindowVisibility = _settings.HideOnStartup ? Visibility.Hidden : Visibility.Visible;
                 Log.Info("|App.OnStartup|End Wox startup ----------------------------------------------------  ");
             });
@@ -165,8 +187,9 @@ namespace Wox
             }
         }
 
-        public void OnSecondAppStarted()
+        public void OnSecondAppStarted(IList<string> args)
         {
+            ParseCommandLineArgs(args);
             Current.MainWindow.Visibility = Visibility.Visible;
         }
     }
