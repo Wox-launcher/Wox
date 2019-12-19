376ecf74865997c3a511e402e0f21bcc9420a868
bao-qian <bao.github@outlook.com>
2019-12-20 04:11:13 +0800

    default version should larger than online version

--- Scripts/post_build.ps1
+++ Scripts/post_build.ps1
@@ -6,7 +6,7 @@ Write-Host "Config: $config"
 
 function Build-Version {
     if ([string]::IsNullOrEmpty($env:APPVEYOR_BUILD_VERSION)) {
-        $v = "1.2.0"
+        $v = "1.4.0"
     } else {
         $v = $env:APPVEYOR_BUILD_VERSION
     }
