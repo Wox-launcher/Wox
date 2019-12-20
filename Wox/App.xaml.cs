3353bba5f22b3929f9b2abf498c1c29bfd541659
bao-qian <bao.github@outlook.com>
2019-12-20 08:42:05 +0800

    handled exception should not be logged

--- Wox/App.xaml.cs
+++ Wox/App.xaml.cs
@@ -143,10 +143,6 @@ namespace Wox
         private static void RegisterAppDomainExceptions()
         {
             AppDomain.CurrentDomain.UnhandledException += ErrorReporting.UnhandledExceptionHandle;
-            AppDomain.CurrentDomain.FirstChanceException += (_, e) =>
-            {
-                Log.Exception("|App.RegisterAppDomainExceptions|First Chance Exception:", e.Exception);
-            };
         }
 
         public void Dispose()
