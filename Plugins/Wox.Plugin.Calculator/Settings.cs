b37669b77e50c937f84e2d211009428c696e0a53
SysC0mp <kevineck93@gmail.com>
2020-02-25 17:45:43 +0100

    Create user control for Calculator settings

---
+++ Plugins/Wox.Plugin.Calculator/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wox.Plugin.Caculator
+{
+    public class Settings
+    {
+        public DecimalSeparator DecimalSeparator { get; set; } = DecimalSeparator.UseSystemLocale;
+    }
+}
---
+++ Plugins/Wox.Plugin.Calculator/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wox.Infrastructure.Storage;
+using Wox.Infrastructure.UserSettings;
+
+namespace Wox.Plugin.Caculator.ViewModels
+{
+    public class SettingsViewModel : BaseModel, ISavable
+    {
+        private readonly PluginJsonStorage<Settings> _storage;
+
+        public SettingsViewModel()
+        {
+            _storage = new PluginJsonStorage<Settings>();
+            Settings = _storage.Load();
+        }
+
+        public Settings Settings { get; set; }
+
+        public void Save()
+        {
+            _storage.Save();
+        }
+    }
+}
