b85f5a147d581f4b031e462d1a15512f5ca872fd
Jeremy Wu <jeremy24wu@gmail.com>
2020-03-23 17:45:42 +1100

    Remove unnecessary interface methods

--- Wox.Core/Configuration/IPortable.cs
+++ Wox.Core/Configuration/IPortable.cs
@@ -3,8 +3,6 @@ namespace Wox.Core.Configuration
 {
     public interface IPortable
     {
-        void EnablePortableMode();
-        void DisablePortableMode();
         void RemoveShortcuts();
         void RemoveUninstallerEntry();
         void CreateShortcuts();
