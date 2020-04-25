acb73b4409ec481e9f43f88d7d6cb8430fc98fe8
bao-qian <bao.github@outlook.com>
2020-04-25 14:04:17 +0800

    fix hello plugin path

--- Scripts/post_build.ps1
+++ Scripts/post_build.ps1
@@ -36,7 +36,7 @@ function Copy-Resources ($path, $config) {
     $target = "$output\$config"
     Copy-Item -Recurse -Force $project\Themes\* $target\Themes\
     Copy-Item -Recurse -Force $project\Images\* $target\Images\
-    Copy-Item -Recurse -Force $path\Plugins\HelloWorldPython $target\Plugins\HelloWorldPython
+    Copy-Item -Recurse -Force $path\Plugins\HelloWorldPython $target\Plugins
     Copy-Item -Recurse -Force $path\JsonRPC $target\JsonRPC
 }
 
