454da67cea7cb338e9d6997f75e145fb34f5017f
sausagemountain <miha.vanya@gmail.com>
2020-04-25 19:22:27 +0500

    check for null in converter

--- Wox/Converters/MultilineTextConverter.cs
+++ Wox/Converters/MultilineTextConverter.cs
@@ -13,13 +13,13 @@ namespace Wox.Converters
         /// <inheritdoc />
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (value as string).Replace("\n", "\u2007");
+            return (value as string)?.Replace("\n", "\u2007") ?? string.Empty;
         }
 
         /// <inheritdoc />
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (value as string).Replace("\u2007", "\n");
+            return (value as string)?.Replace("\u2007", "\n") ?? string.Empty;
         }
     }
 }
