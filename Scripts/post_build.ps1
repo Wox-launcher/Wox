4667f18d77d50d880e1382f13a12a0052c0cbaef
bao-qian <bao.github@outlook.com>
2020-04-25 11:56:20 +0800

    upgrade squirrel version

--- Scripts/post_build.ps1
+++ Scripts/post_build.ps1
@@ -98,7 +98,7 @@ function Pack-Squirrel-Installer ($path, $version, $output) {
     $icon = "$path\Wox\Resources\app.ico"
     Write-Host "icon: $icon"
     # Squirrel.com: https://github.com/Squirrel/Squirrel.Windows/issues/369
-    New-Alias Squirrel $env:USERPROFILE\.nuget\packages\squirrel.windows\1.5.2\tools\Squirrel.exe -Force
+    New-Alias Squirrel $env:USERPROFILE\.nuget\packages\squirrel.windows\1.9.1\tools\Squirrel.exe -Force
     # why we need Write-Output: https://github.com/Squirrel/Squirrel.Windows/issues/489#issuecomment-156039327
     # directory of releaseDir in squirrel can't be same as directory ($nupkg) in releasify
     $temp = "$output\Temp"
