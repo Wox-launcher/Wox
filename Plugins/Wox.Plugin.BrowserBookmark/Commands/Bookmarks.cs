0c165c8f2ae0d47fbb5395a2b2ba9c376b622909
bao-qian <bao.github@outlook.com>
2020-04-26 12:37:01 +0800

    disable pinyin for browser bookmarks
    1. pinyin library will cause around 200mb memory usage on startup for DEBUG config, and released immediately
    2. there will be a unified pinyin module in future
    3. this will also reduce startup cost

--- Plugins/Wox.Plugin.BrowserBookmark/Commands/Bookmarks.cs
+++ Plugins/Wox.Plugin.BrowserBookmark/Commands/Bookmarks.cs
@@ -9,7 +9,7 @@ namespace Wox.Plugin.BrowserBookmark.Commands
         internal static bool MatchProgram(Bookmark bookmark, string queryString)
         {
             if (StringMatcher.FuzzySearch(queryString, bookmark.Name).IsSearchPrecisionScoreMet()) return true;
-            if (StringMatcher.FuzzySearch(queryString, bookmark.PinyinName).IsSearchPrecisionScoreMet()) return true;
+            //if (StringMatcher.FuzzySearch(queryString, bookmark.PinyinName).IsSearchPrecisionScoreMet()) return true;
             if (StringMatcher.FuzzySearch(queryString, bookmark.Url).IsSearchPrecisionScoreMet()) return true;
 
             return false;
