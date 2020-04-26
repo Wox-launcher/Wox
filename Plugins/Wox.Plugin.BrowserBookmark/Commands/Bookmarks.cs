f92e34efd704a73b3ca333f1740d3db1d3b1111c
bao-qian <bao.github@outlook.com>
2020-04-26 15:04:24 +0800

    lazy load firefox bookmark

--- Plugins/Wox.Plugin.BrowserBookmark/Commands/Bookmarks.cs
+++ Plugins/Wox.Plugin.BrowserBookmark/Commands/Bookmarks.cs
@@ -15,17 +15,5 @@ namespace Wox.Plugin.BrowserBookmark.Commands
             return false;
         }
 
-        internal static List<Bookmark> LoadAllBookmarks()
-        {
-            var chromeBookmarks = new ChromeBookmarks();
-            var mozBookmarks = new FirefoxBookmarks();
-
-            //TODO: Let the user select which browser's bookmarks are displayed
-            
-            var b1 = mozBookmarks.GetBookmarks();
-            var b2 = chromeBookmarks.GetBookmarks();
-            b1.AddRange(b2);
-            return b1.Distinct().ToList();
-        }
     }
 }
--- Plugins/Wox.Plugin.BrowserBookmark/Main.cs
+++ Plugins/Wox.Plugin.BrowserBookmark/Main.cs
@@ -6,24 +6,44 @@ using Wox.Plugin.BrowserBookmark.Commands;
 using Wox.Plugin.BrowserBookmark.Models;
 using Wox.Plugin.BrowserBookmark.Views;
 using Wox.Infrastructure;
+using System.Threading.Tasks;
 
 namespace Wox.Plugin.BrowserBookmark
 {
     public class Main : ISettingProvider, IPlugin, IReloadable, IPluginI18n, ISavable
     {
         private PluginInitContext context;
-        
+
         private List<Bookmark> cachedBookmarks = new List<Bookmark>();
+        private object _updateLock = new object();
 
         private readonly Settings _settings;
         private readonly PluginJsonStorage<Settings> _storage;
+
 
         public Main()
         {
             _storage = new PluginJsonStorage<Settings>();
             _settings = _storage.Load();
 
-            cachedBookmarks = Bookmarks.LoadAllBookmarks();
+            //TODO: Let the user select which browser's bookmarks are displayed
+            var chromeBookmarks = new ChromeBookmarks().GetBookmarks().Distinct().ToList();
+            lock (_updateLock)
+            {
+                cachedBookmarks = chromeBookmarks;
+            }
+            Task.Run(() =>
+            {
+                // firefox bookmarks is slow, since it nened open sqlite connection.
+                // use lazy load
+                var mozBookmarks = new FirefoxBookmarks().GetBookmarks();
+                var cached = mozBookmarks.Concat(cachedBookmarks).Distinct().ToList();
+                lock (_updateLock)
+                {
+                    cachedBookmarks = cached;
+                }
+            });
+
         }
 
         public void Init(PluginInitContext context)
@@ -37,43 +57,57 @@ namespace Wox.Plugin.BrowserBookmark
 
             // Should top results be returned? (true if no search parameters have been passed)
             var topResults = string.IsNullOrEmpty(param);
-            
-            var returnList = cachedBookmarks;
 
-            if (!topResults)
+            lock (_updateLock)
             {
-                // Since we mixed chrome and firefox bookmarks, we should order them again                
-                returnList = cachedBookmarks.Where(o => Bookmarks.MatchProgram(o, param)).ToList();
-                returnList = returnList.OrderByDescending(o => o.Score).ToList();
-            }
-            
-            return returnList.Select(c => new Result()
-            {
-                Title = c.Name,
-                SubTitle = c.Url,
-                IcoPath = @"Images\bookmark.png",
-                Score = 5,
-                Action = (e) =>
+
+                var returnList = cachedBookmarks;
+
+                if (!topResults)
                 {
-                    if (_settings.OpenInNewBrowserWindow)
-                    {
-                        c.Url.NewBrowserWindow(_settings.BrowserPath);
-                    }
-                    else
-                    {
-                        c.Url.NewTabInBrowser(_settings.BrowserPath);
-                    }
-
-                    return true;
+                    // Since we mixed chrome and firefox bookmarks, we should order them again                
+                    returnList = cachedBookmarks.Where(o => Bookmarks.MatchProgram(o, param)).ToList();
+                    returnList = returnList.OrderByDescending(o => o.Score).ToList();
                 }
-            }).ToList();
+
+                var results = returnList.Select(c => new Result()
+                {
+                    Title = c.Name,
+                    SubTitle = c.Url,
+                    IcoPath = @"Images\bookmark.png",
+                    Score = 5,
+                    Action = (e) =>
+                    {
+                        if (_settings.OpenInNewBrowserWindow)
+                        {
+                            c.Url.NewBrowserWindow(_settings.BrowserPath);
+                        }
+                        else
+                        {
+                            c.Url.NewTabInBrowser(_settings.BrowserPath);
+                        }
+
+                        return true;
+                    }
+                }).ToList();
+                return results;
+            }
+
         }
 
         public void ReloadData()
         {
-            cachedBookmarks.Clear();
-
-            cachedBookmarks = Bookmarks.LoadAllBookmarks();
+            //TODO: Let the user select which browser's bookmarks are displayed
+            var chromeBookmarks = new ChromeBookmarks();
+            var mozBookmarks = new FirefoxBookmarks();
+            var b1 = mozBookmarks.GetBookmarks();
+            var b2 = chromeBookmarks.GetBookmarks();
+            b1.AddRange(b2);
+            var cached = b1.Distinct().ToList();
+            lock (_updateLock)
+            {
+                cachedBookmarks = cached;
+            }
         }
 
         public string GetTranslatedPluginTitle()
