9e8296ab2477c90bd981fbff0984bd0c48cd00e7
bao-qian <bao.github@outlook.com>
2020-04-29 14:28:39 +0800

    fix appveyor build

--- Wox.Installer/post_build.ps1
+++ Wox.Installer/post_build.ps1
@@ -7,4 +7,4 @@ $path = "$solution\Output\packages"
 $installer = "$path\Wox-Full-Installer.exe"
 $version = (Get-Command $installer).FileVersionInfo.ProductVersion
 $newName = "$path\Wox-Full-Installer.$version.exe"
-Move-Item $installer $newName
+Move-Item -Force $installer $newName
