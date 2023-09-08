1c98e1fe1b3deb719fd4843b5dc752920f34ba65
lylwx <sudalyl@gmail.com>
2023-09-08 12:24:20 +0800

    WIP: ResultListBox

---
+++ Wox/ViewModels/ResultListBoxViewModel.cs
@@ -0,0 +1,6 @@
+namespace Wox.ViewModels;
+
+public class ResultListBoxViewModel : ViewModelBase
+{
+    public string[] Animals { get; set; } =  { "Dog", "Cat", "Fish" };
+}
---
+++ Wox/Views/ResultListBox.axaml.cs
@@ -0,0 +1,12 @@
+using System;
+using Avalonia.Controls;
+
+namespace Wox.Views;
+
+public partial class ResultListBox :Control
+{
+    public ResultListBox()
+    {
+        InitializeComponent();
+    }
+}
