471ff0f63ca1477caa4d503fcce1a23d699b6edf
qianlifeng <qianlf2010@gmail.com>
2023-10-09 13:51:53 +0800

    Improve ui structure

--- Wox/ui/ui_impl.go
+++ Wox/ui/ui_impl.go
@@ -2,8 +2,11 @@ package ui
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/google/uuid"
+	"time"
+	"wox/plugin"
 	"wox/util"
 )
 
@@ -47,4 +50,89 @@ func (u *uiImpl) send(ctx context.Context, method string, params map[string]stri
 		Method: method,
 		Params: params,
 	})
+}
+
+func onUIRequest(ctx context.Context, request websocketRequest) {
+	switch request.Method {
+	case "Query":
+		handleQuery(ctx, request)
+	case "Action":
+		handleAction(ctx, request)
+	case "RegisterMainHotkey":
+		handleRegisterMainHotkey(ctx, request)
+	}
+}
+
+func handleQuery(ctx context.Context, request websocketRequest) {
+	query, ok := request.Params["query"]
+	if !ok {
+		logger.Error(ctx, "query parameter not found")
+		return
+	}
+
+	resultChan, doneChan := plugin.GetPluginManager().Query(ctx, plugin.NewQuery(query))
+	select {
+	case results := <-resultChan:
+		logger.Info(ctx, fmt.Sprintf("query result count: %d", len(results)))
+		if len(results) == 0 {
+			return
+		}
+
+		response := websocketResponse{
+			Id:     request.Id,
+			Method: request.Method,
+			Data:   plugin.NewQueryResultForUIs(results),
+		}
+
+		marshalData, marshalErr := json.Marshal(response)
+		if marshalErr != nil {
+			logger.Error(ctx, fmt.Sprintf("failed to marshal websocket response: %s", marshalErr.Error()))
+			return
+		}
+
+		m.Broadcast(marshalData)
+	case <-doneChan:
+		logger.Info(ctx, "query done")
+	case <-time.After(time.Second * 30):
+		logger.Info(ctx, fmt.Sprintf("query timeout, query: %s, request id: %s", query, request.Id))
+	}
+}
+
+func handleAction(ctx context.Context, request websocketRequest) {
+	resultId, ok := request.Params["id"]
+	if !ok {
+		logger.Error(ctx, "id parameter not found")
+		return
+	}
+
+	action := plugin.GetActionForResult(resultId)
+	if action == nil {
+		logger.Error(ctx, fmt.Sprintf("action not found for result id: %s", resultId))
+		return
+	}
+
+	action()
+}
+
+func handleRegisterMainHotkey(ctx context.Context, request websocketRequest) {
+	hotkey, ok := request.Params["hotkey"]
+	if !ok {
+		logger.Error(ctx, "hotkey parameter not found")
+		return
+	}
+
+	registerErr := GetUIManager().RegisterMainHotkey(ctx, hotkey)
+	if registerErr != nil {
+		responseUI(ctx, websocketResponse{
+			Id:     request.Id,
+			Method: request.Method,
+			Data:   registerErr.Error(),
+		})
+	} else {
+		responseUI(ctx, websocketResponse{
+			Id:     request.Id,
+			Method: request.Method,
+			Data:   "success",
+		})
+	}
 }
--- Wox/ui/websocket.go
+++ Wox/ui/websocket.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	"github.com/olahol/melody"
 	"net/http"
-	"time"
-	"wox/plugin"
 	"wox/util"
 )
 
@@ -48,14 +46,7 @@ func serveAndWait(ctx context.Context, port int) {
 		}
 
 		util.Go(ctxNew, "handle ui query", func() {
-			switch request.Method {
-			case "query":
-				handleQuery(ctxNew, request)
-			case "action":
-				handleAction(ctxNew, request)
-			case "registerMainHotkey":
-				handleRegisterMainHotkey(ctxNew, request)
-			}
+			onUIRequest(ctxNew, request)
 		})
 	})
 
@@ -63,80 +54,6 @@ func serveAndWait(ctx context.Context, port int) {
 	err := http.ListenAndServe(fmt.Sprintf("localhost:%d", port), nil)
 	if err != nil {
 		logger.Error(ctx, fmt.Sprintf("failed to start server: %s", err.Error()))
-	}
-}
-
-func handleQuery(ctx context.Context, request websocketRequest) {
-	query, ok := request.Params["query"]
-	if !ok {
-		logger.Error(ctx, "query parameter not found")
-		return
-	}
-
-	resultChan, doneChan := plugin.GetPluginManager().Query(ctx, plugin.NewQuery(query))
-	select {
-	case results := <-resultChan:
-		logger.Info(ctx, fmt.Sprintf("query result count: %d", len(results)))
-		if len(results) == 0 {
-			return
-		}
-
-		response := websocketResponse{
-			Id:     request.Id,
-			Method: request.Method,
-			Data:   plugin.NewQueryResultForUIs(results),
-		}
-
-		marshalData, marshalErr := json.Marshal(response)
-		if marshalErr != nil {
-			logger.Error(ctx, fmt.Sprintf("failed to marshal websocket response: %s", marshalErr.Error()))
-			return
-		}
-
-		m.Broadcast(marshalData)
-	case <-doneChan:
-		logger.Info(ctx, "query done")
-	case <-time.After(time.Second * 30):
-		logger.Info(ctx, fmt.Sprintf("query timeout, query: %s, request id: %s", query, request.Id))
-	}
-}
-
-func handleAction(ctx context.Context, request websocketRequest) {
-	resultId, ok := request.Params["id"]
-	if !ok {
-		logger.Error(ctx, "id parameter not found")
-		return
-	}
-
-	action := plugin.GetActionForResult(resultId)
-	if action == nil {
-		logger.Error(ctx, fmt.Sprintf("action not found for result id: %s", resultId))
-		return
-	}
-
-	action()
-}
-
-func handleRegisterMainHotkey(ctx context.Context, request websocketRequest) {
-	hotkey, ok := request.Params["hotkey"]
-	if !ok {
-		logger.Error(ctx, "hotkey parameter not found")
-		return
-	}
-
-	registerErr := GetUIManager().RegisterMainHotkey(ctx, hotkey)
-	if registerErr != nil {
-		responseUI(ctx, websocketResponse{
-			Id:     request.Id,
-			Method: request.Method,
-			Data:   registerErr.Error(),
-		})
-	} else {
-		responseUI(ctx, websocketResponse{
-			Id:     request.Id,
-			Method: request.Method,
-			Data:   "success",
-		})
 	}
 }
 
