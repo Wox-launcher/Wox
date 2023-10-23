9ab5eabc0bcb4a8d0d33cf3205fcff9ef708c9ac
qianlifeng <qianlf2010@gmail.com>
2023-10-23 14:37:06 +0800

    Remove unrelated project

--- Wox.UI.React/.umirc.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "umi";
-
-export default defineConfig({
-  routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
-  ],
-  npmClient: 'yarn',
-});
--- Wox.UI.React/src/enums/WoxMessageMethodEnum.ts
+++
@@ -1,7 +0,0 @@
-import {BaseEnum} from "./base/BaseEnum.ts";
-
-export class WoxMessageMethodEnum extends BaseEnum {
-    static readonly QUERY = WoxMessageMethodEnum.define("Query", "查询");
-    static readonly ACTION = WoxMessageMethodEnum.define("Action", "执行");
-    static readonly REGISTERMAINHOTKEY = WoxMessageMethodEnum.define("RegisterMainHotkey", "注册快捷键");
-}
