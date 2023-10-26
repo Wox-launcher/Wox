12ff4ce7716fb7e0c6a691f2253d00ac9d8015ba
qianlifeng <qianlf2010@gmail.com>
2023-10-26 22:41:34 +0800

    Fix compile error

--- Wox.UI.Tauri/src-tauri/src/main.rs
+++ Wox.UI.Tauri/src-tauri/src/main.rs
@@ -5,12 +5,13 @@
 extern crate log;
 extern crate simplelog;
 
-use std::env;
-use std::path::PathBuf;
 use simplelog::*;
+use std::env;
 use std::fs::File;
-
-mod spotlight;
+use std::path::PathBuf;
+use tauri::Manager;
+use tauri_nspanel::{panel_delegate, WindowExt};
+use tauri_nspanel::cocoa::appkit::{NSMainMenuWindowLevel, NSWindowCollectionBehavior};
 
 #[tauri::command]
 fn get_server_port() -> String {
@@ -31,37 +32,44 @@ fn log_ui(msg: String) {
 
 fn init_log_file() {
     if let Some(home_dir) = dirs::home_dir() {
-       let mut base_path = PathBuf::new();
-       base_path.push(home_dir);
-       base_path.push(".wox");
-       base_path.push("log");
-       base_path.push("ui.log");
-       CombinedLogger::init(
-               vec![
-                   TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
-                   WriteLogger::new(LevelFilter::Info, Config::default(), File::create(base_path).unwrap()),
-               ]
-           ).unwrap();
-   } else {
-       println!("Can not find user main home path");
-   }
+        let mut base_path = PathBuf::new();
+        base_path.push(home_dir);
+        base_path.push(".wox");
+        base_path.push("log");
+        base_path.push("ui.log");
+        CombinedLogger::init(
+            vec![
+                TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
+                WriteLogger::new(LevelFilter::Info, Config::default(), File::create(base_path).unwrap()),
+            ]
+        ).unwrap();
+    } else {
+        println!("Can not find user main home path");
+    }
 }
 
 fn main() {
     init_log_file();
-
     tauri::Builder::default()
-           .invoke_handler(tauri::generate_handler![
-               spotlight::init_spotlight_window,
-               get_server_port,
-               log_ui
-           ])
-           .manage(spotlight::State::default())
-           .setup(move |app| {
-               // Set activation poicy to Accessory to prevent the app icon from showing on the dock
-               app.set_activation_policy(tauri::ActivationPolicy::Accessory);
-               Ok(())
-           })
-           .run(tauri::generate_context!())
-           .expect("error while running tauri application");
+        .plugin(tauri_nspanel::init())
+        .invoke_handler(tauri::generate_handler![get_server_port, log_ui])
+        .setup(|app| {
+            if cfg!(target_os = "macos") {
+                // hide the dock icon
+                app.set_activation_policy(tauri::ActivationPolicy::Accessory);
+
+                let window = app.get_window("main").unwrap();
+                let panel = window.to_panel().unwrap();
+                // Set panel above the main menu window level
+                panel.set_level(NSMainMenuWindowLevel + 1);
+                // Ensure that the panel can display over the top of fullscreen apps
+                panel.set_collection_behaviour(NSWindowCollectionBehavior::NSWindowCollectionBehaviorTransient
+                    | NSWindowCollectionBehavior::NSWindowCollectionBehaviorMoveToActiveSpace
+                );
+            }
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
