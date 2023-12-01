696eea735a22394e6f8bbdd574c7f6a66cc30c95
qianlifeng <qianlf2010@gmail.com>
2023-12-01 14:22:18 +0800

    Remove flutter manner

--- Wox.UI.Flutter/wox/lib/main.dart
+++ Wox.UI.Flutter/wox/lib/main.dart
@@ -47,6 +47,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: WoxApp(),
     );
   }
