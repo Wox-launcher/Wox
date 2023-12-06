8f4084a54ef90ca17a028eca9dc3de5f5ada3a6e
qianlifeng <qianlf2010@gmail.com>
2023-12-06 21:34:31 +0800

    Fix linux build error

---
+++ Wox/plugin/system/app/app_linux.go
@@ -0,0 +1,32 @@
+package app
+
+import (
+	"context"
+	"errors"
+	"wox/plugin"
+	"wox/util"
+)
+
+var appRetriever = &LinuxRetriever{}
+
+type LinuxRetriever struct {
+	api plugin.API
+}
+
+func (a *LinuxRetriever) GetPlatform() string {
+	return util.PlatformLinux
+}
+
+func (a *LinuxRetriever) GetAppDirectories(ctx context.Context) []appDirectory {
+	return []appDirectory{
+		{},
+	}
+}
+
+func (a *LinuxRetriever) GetAppExtensions(ctx context.Context) []string {
+	return []string{}
+}
+
+func (a *LinuxRetriever) ParseAppInfo(ctx context.Context, path string) (appInfo, error) {
+	return appInfo{}, errors.New("not implemented")
+}
