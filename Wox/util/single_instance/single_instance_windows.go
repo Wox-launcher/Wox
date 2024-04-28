747a1217529b6bc8000e0b5349c9b08bf9e79a08
qianlifeng <qianlf2010@gmail.com>
2024-04-28 20:56:33 +0800

    Fix build error on windows

--- Wox/util/single_instance/single_instance_windows.go
+++ Wox/util/single_instance/single_instance_windows.go
@@ -5,8 +5,30 @@ package single_instance
 import (
 	"os"
 	"syscall"
+	"unsafe"
 	"wox/util"
 )
+
+const (
+	LOCKFILE_EXCLUSIVE_LOCK = 2
+)
+
+var (
+	kernel32       = syscall.MustLoadDLL("kernel32.dll")
+	procLockFileEx = kernel32.MustFindProc("LockFileEx")
+)
+
+func LockFileEx(hFile syscall.Handle, dwFlags, dwReserved, nNumberOfBytesToLockLow, nNumberOfBytesToLockHigh uint32, lpOverlapped *syscall.Overlapped) (err error) {
+	r1, _, e1 := syscall.Syscall6(procLockFileEx.Addr(), 6, uintptr(hFile), uintptr(dwFlags), uintptr(dwReserved), uintptr(nNumberOfBytesToLockLow), uintptr(nNumberOfBytesToLockHigh), uintptr(unsafe.Pointer(lpOverlapped)))
+	if r1 == 0 {
+		if e1 != 0 {
+			err = error(e1)
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	return
+}
 
 func lock(content string) error {
 	filename := util.GetLocation().GetAppLockFilePath()
@@ -17,7 +39,7 @@ func lock(content string) error {
 	defer file.Close()
 
 	var overlapped syscall.Overlapped
-	err = syscall.LockFileEx(syscall.Handle(file.Fd()), syscall.LOCKFILE_EXCLUSIVE_LOCK, 0, 1, 0, &overlapped)
+	err = LockFileEx(syscall.Handle(file.Fd()), LOCKFILE_EXCLUSIVE_LOCK, 0, 1, 0, &overlapped)
 	if err != nil {
 		return err
 	}
