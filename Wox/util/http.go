6f3dc6def537a615ff0fc8670efb8bf33d926826
qianlifeng <qianlf2010@gmail.com>
2024-10-17 21:44:49 +0800

    chore: Refactor HTTP client to reuse instance

--- Wox/util/http.go
+++ Wox/util/http.go
@@ -2,12 +2,14 @@ package util
 
 import (
 	"context"
+
 	"github.com/go-resty/resty/v2"
 )
 
+var client *resty.Client
+
 func HttpGet(ctx context.Context, url string) ([]byte, error) {
-	client := resty.New()
-	resp, err := client.R().Get(url)
+	resp, err := getClient().R().Get(url)
 	if err != nil {
 		return nil, err
 	}
@@ -19,8 +21,7 @@ func HttpGet(ctx context.Context, url string) ([]byte, error) {
 }
 
 func HttpPost(ctx context.Context, url string, body any) ([]byte, error) {
-	client := resty.New()
-	resp, err := client.R().SetBody(body).Post(url)
+	resp, err := getClient().R().SetBody(body).Post(url)
 	if err != nil {
 		return nil, err
 	}
@@ -32,8 +33,7 @@ func HttpPost(ctx context.Context, url string, body any) ([]byte, error) {
 }
 
 func HttpDownload(ctx context.Context, url string, dest string) error {
-	client := resty.New()
-	resp, err := client.R().SetOutput(dest).Get(url)
+	resp, err := getClient().R().SetOutput(dest).Get(url)
 	if err != nil {
 		return err
 	}
@@ -42,4 +42,13 @@ func HttpDownload(ctx context.Context, url string, dest string) error {
 	}
 
 	return nil
+}
+
+func getClient() *resty.Client {
+	if client == nil {
+		client = resty.New()
+		client.SetHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+	}
+
+	return client
 }
