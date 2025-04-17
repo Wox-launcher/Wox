9d80c2d848dcaf6030a1be3acbe9cefe5b7ee5bc
Weifeng Liu <weifeng.liu@intel.com>
2025-04-17 16:49:46 +0800

    feat(ui): Get screen size using gtk APIs on Linux
    - In favor of gtk APIs for screen size query since this way considers DPI of the screen.
    - Fall back to X11 APIs when failing to call gtk APIs.

--- wox.core/util/screen/screen_linux.go
+++ wox.core/util/screen/screen_linux.go
@@ -1,5 +1,10 @@
 package screen
 
+import (
+	"github.com/gotk3/gotk3/gdk"
+	"github.com/gotk3/gotk3/gtk"
+)
+
 /*
 #cgo LDFLAGS: -lX11
 #include <X11/Xlib.h>
@@ -21,7 +26,36 @@ void closeDisplay(Display* display) {
 */
 import "C"
 
+func GetMouseScreenGtk() (Size, error) {
+	err := gtk.InitCheck(nil)
+	if err != nil {
+		return Size{}, err
+	}
+
+	default_gdk_display, err := gdk.DisplayGetDefault()
+	if err != nil {
+		return Size{}, err
+	}
+
+	monitor, err := default_gdk_display.GetPrimaryMonitor()
+	if err != nil {
+		return Size{}, err
+	}
+
+	area := monitor.GetWorkarea()
+	return Size{
+		Width:  int(area.GetWidth()),
+		Height: int(area.GetHeight()),
+	}, nil
+}
+
 func GetMouseScreen() Size {
+	// Give gtk a try, as it considers DPI and scaling of the screen
+	size, err := GetMouseScreenGtk()
+	if err == nil {
+		return size
+	}
+	// Fallback to X11
 	display := C.openDisplay()
 	if display == nil {
 		panic("Could not open X11 display")
