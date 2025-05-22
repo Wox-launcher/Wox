a3bad11da706daee90ac817122cdb2bdebc864f2
qianlifeng <qianlf2010@gmail.com>
2025-05-22 20:40:48 +0800

    feat(ui): update flutter dependencies
    - Added file_picker plugin to GeneratedPluginRegistrant.swift and Podfile.lock.
    - Updated pubspec.yaml to include the latest version of syncfusion_flutter_pdfviewer.
    - Updated various dependencies in pubspec.lock to their latest versions.

--- wox.ui.flutter/wox/macos/Flutter/GeneratedPluginRegistrant.swift
+++ wox.ui.flutter/wox/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import desktop_drop
 import device_info_plus
+import file_picker
 import flutter_inappwebview_macos
 import hotkey_manager_macos
 import macos_window_utils
@@ -18,6 +19,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   HotkeyManagerMacosPlugin.register(with: registry.registrar(forPlugin: "HotkeyManagerMacosPlugin"))
   MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
