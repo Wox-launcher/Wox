bc24dfa2234329b9dad04820db8bf22cb5bedb5e
qianlifeng <qianlf2010@gmail.com>
2026-05-31 20:34:54 +0800

    feat(query-toolbar): add drag-and-drop functionality to toolbar actions

--- wox.ui.flutter/wox/lib/modules/launcher/views/wox_query_toolbar_view.dart
+++ wox.ui.flutter/wox/lib/modules/launcher/views/wox_query_toolbar_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:uuid/v4.dart';
 import 'package:wox/components/wox_hotkey_view.dart';
+import 'package:wox/components/wox_drag_move_view.dart';
 import 'package:wox/components/wox_image_view.dart';
 import 'package:wox/controllers/wox_launcher_controller.dart';
 import 'package:wox/entity/wox_hotkey.dart';
@@ -195,7 +196,19 @@ class WoxQueryToolbarView extends GetView<WoxLauncherController> {
             actionWidgets.add(_buildClickableToolbarAction(actionInfo, hotkey));
           }
 
-          return Align(alignment: Alignment.centerRight, child: Row(mainAxisSize: MainAxisSize.min, children: actionWidgets));
+          return Row(
+            children: [
+              Expanded(
+                child: WoxDragMoveArea(
+                  child: Container(color: Colors.transparent),
+                  onDragEnd: () {
+                    controller.focusQueryBox();
+                  },
+                ),
+              ),
+              Row(mainAxisSize: MainAxisSize.min, children: actionWidgets),
+            ],
+          );
         },
       );
     });
