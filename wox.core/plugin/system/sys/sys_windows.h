7139eb28b3578b61b0d69c337e2125a83b06b251
qianlifeng <qianlf2010@gmail.com>
2026-06-15 21:14:15 +0800

    feat: add task view functionality across platforms with localization support

--- wox.core/plugin/system/sys/sys_windows.h
+++ wox.core/plugin/system/sys/sys_windows.h
@@ -11,6 +11,7 @@ HRESULT wox_sys_set_master_volume(float level);
 HRESULT wox_sys_volume_step_up(void);
 HRESULT wox_sys_volume_step_down(void);
 HRESULT wox_sys_toggle_mute(void);
+HRESULT wox_sys_show_task_view(void);
 
 #ifdef __cplusplus
 }
