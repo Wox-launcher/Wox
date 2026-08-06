c607643f1ded7e268215e870d14177ee1bf61fcd
qianlifeng <qianlf2010@gmail.com>
2026-08-06 21:52:39 +0800

    feat(ui): improve ui on windows

--- wox.core/ui/runtime/renderer_windows.cpp
+++ wox.core/ui/runtime/renderer_windows.cpp
@@ -484,7 +484,7 @@ extern "C" int32_t wox_renderer_draw_text(WoxRenderer *renderer, const char *tex
 
   const D2D1_COLOR_F color = make_color(red, green, blue, alpha);
   renderer->brush->SetColor(color);
-  renderer->d2d_context->DrawTextLayout(D2D1::Point2F(x, y), layout, renderer->brush, D2D1_DRAW_TEXT_OPTIONS_CLIP);
+  renderer->d2d_context->DrawTextLayout(D2D1::Point2F(x, y), layout, renderer->brush, static_cast<D2D1_DRAW_TEXT_OPTIONS>(D2D1_DRAW_TEXT_OPTIONS_CLIP | D2D1_DRAW_TEXT_OPTIONS_ENABLE_COLOR_FONT));
   layout->Release();
   return S_OK;
 }
